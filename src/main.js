62b73c17eafe0f4ab2e8bb70f1bfa33b18441297
Lee Chase <lee.chase@uk.ibm.com>
2019-08-12 11:38:14 +0100

    chore: prettier changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import CarbonComponentsVue from "@carbon/vue";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import CarbonComponentsVue from '@carbon/vue';
 Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false;
@@ -9,4 +9,4 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
