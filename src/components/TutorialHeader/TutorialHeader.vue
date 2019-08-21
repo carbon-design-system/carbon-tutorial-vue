30b2368132418a21d359c005ef35052143f6ca83
Michael Herwig <michael.herwig@ibm.com>
2019-08-21 18:47:16 +0200

    Header defined

---
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <cv-header aria-label="Carbon tutorial">
+    <cv-skip-to-content href="#main-content"
+      >Skip to content</cv-skip-to-content
+    >
+
+    <cv-header-name href="/" prefix="IBM">Carbon Tutorial</cv-header-name>
+
+    <cv-header-nav>
+      <cv-header-menu-item href="/repos">Repositories</cv-header-menu-item>
+    </cv-header-nav>
+
+    <template slot="header-global" />
+  </cv-header>
+</template>
