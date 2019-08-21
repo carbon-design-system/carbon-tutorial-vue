7304dc78adfdab59dfc0e9b3328560db7b3b5fe5
Michael Herwig <michael.herwig@ibm.com>
2019-08-21 18:59:33 +0200

    Header added

--- src/components/TutorialHeader/TutorialHeader.vue
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -10,6 +10,28 @@
       <cv-header-menu-item href="/repos">Repositories</cv-header-menu-item>
     </cv-header-nav>
 
-    <template slot="header-global" />
+    <template slot="header-global">
+        <cv-header-global-action aria-label="Notifications">
+            <notification-20 />
+        </cv-header-global-action>
+        <cv-header-global-action aria-label="User avatar">
+            <user-avatar-20 />
+        </cv-header-global-action>
+        <cv-header-global-action aria-label="App switcher">
+            <app-switcher-20 />
+        </cv-header-global-action>
+    </template>
+
   </cv-header>
 </template>
+
+<script>
+import Notification20 from "@carbon/icons-vue/es/notification/20";
+import UserAvatar20 from "@carbon/icons-vue/es/user--avatar/20";
+import AppSwitcher20 from "@carbon/icons-vue/es/app-switcher/20";
+
+export default {
+  name: "TutorialHeader",
+  components: { Notification20, UserAvatar20, AppSwitcher20 }
+};
+</script>
