97a92845d907071dd35a31ed724d763c20c9c42b
unknown <min-chi_huang@tw.ibm.com>
2019-08-22 18:06:40 +0800

    feat(tutorial): complete step 3

--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,12 +1,28 @@
 <template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
+  <div v-if="loading">
+	<cv-data-table-skeleton
+	  v-if="loading"
+	  :columns="columns"
+	  :title="title"
+	  :helper-text="helperText"
+	  :rows="10"
+	/>
+  </div>
+  <cv-data-table v-else :columns="columns"
+    :title="title"
+    :helper-text="helperText"
+    :pagination="{ numberOfItems: this.totalRows }"
+    @pagination="$emit('pagination', $event)">
     <template slot="data">
       <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
-        <cv-data-table-cell
-          v-for="(cell, cellIndex) in row.data"
-          :key="`${cellIndex}`"
-          >{{ cell }}</cv-data-table-cell
-        >
+
+		<cv-data-table-cell v-for="(cell, cellIndex) in row.data" :key="`${cellIndex}`">
+		  <template v-if="!cell.url">
+			{{cell}}
+		  </template>
+		  <link-list v-else :url="cell.url" :homepage-url="cell.homepageUrl" />
+		</cv-data-table-cell>
+
         <template slot="expandedContent">{{ row.description }}</template>
       </cv-data-table-row>
     </template>
@@ -14,14 +30,22 @@
 </template>
 
 <script>
+import LinkList from './LinkList';
+
 export default {
   name: 'RepoTable',
+
   props: {
     headers: Array,
     rows: Array,
     title: String,
-    helperText: String
+    helperText: String,
+    loading: Boolean,
+	totalRows: Number,
   },
+  
+  components: { LinkList },
+  
   computed: {
     columns() {
       return this.headers.map(header => header.header);
@@ -36,7 +60,7 @@ export default {
           row.stars,
           row.links
         ],
-        description: 'Row description'
+        description: row.description
       }));
     }
   }
