53851d13628992acf522b79c96d7a91767eb9597
CARLOS EDUARDO EUGENIO DOS SANTOS <cesantos@br.ibm.com>
2019-08-22 15:55:57 -0300

    feat(tutorial): complete step 3

---
+++ src/createProvider.js
@@ -0,0 +1,26 @@
+import VueApollo from 'vue-apollo';
+import { createApolloClient } from 'vue-cli-plugin-apollo/graphql-client';
+import { defaultOptions } from './vue-apollo';
+// Call this in the Vue app file
+export function createProvider(options = {}) {
+  // Create apollo client
+  const { apolloClient, wsClient } = createApolloClient({
+    ...defaultOptions,
+    ...options,
+  });
+  apolloClient.wsClient = wsClient;
+  // Create vue apollo provider
+  const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+    defaultOptions: {
+      $query: {
+        // fetchPolicy: 'cache-and-network',
+      },
+    },
+    errorHandler(error) {
+      // eslint-disable-next-line no-console
+      console.log('%cError', 'background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;', error.message);
+    },
+  });
+  return apolloProvider;
+}
