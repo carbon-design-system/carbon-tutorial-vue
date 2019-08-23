b56678ad34b4f32265f3d55132fdc8332098ff93
Michael Herwig <michael.herwig@outlook.de>
2019-08-23 15:47:59 +0200

    Grid added

--- src/views/LandingPage/LandingPage.vue
+++ src/views/LandingPage/LandingPage.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>LANDING PAGE</div>
+  <div class="bx--grid bx--grid--full-width landing-page">
+    <div class="bx--row landing-page__banner">
+      <div class="bx--col-lg-16">1</div>
+    </div>
+    <div class="bx--row landing-page__r2">
+      <div class="bx--col-md-4 bx--col-lg-7">7/16</div>
+      <div class="bx--col-md-4 bx--offset-lg-1 bx--col-lg-8">8/16</div>
+    </div>
+    <div class="bx--row landing-page__r3">
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+    </div>
+  </div>
 </template>
