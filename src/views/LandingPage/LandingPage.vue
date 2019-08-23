30334ff73261a7f850fd4de11133a3b39e935ec8
Michael Herwig <michael.herwig@outlook.de>
2019-08-23 16:43:17 +0200

    Main content added

--- src/views/LandingPage/LandingPage.vue
+++ src/views/LandingPage/LandingPage.vue
@@ -17,18 +17,35 @@
         <h1 class="landing-page__heading">Design &amp; build with Carbon</h1>
       </div>
     </div>
-    <!--Content-->
+    <!--Main Section-->
     <div class="bx--row landing-page__r2">
       <div class="bx--col bx--no-gutter">
         <cv-tabs selected="0" aria-label="Tab navigation">
           <cv-tab label="About">
             <div class="bx--grid bx--grid--no-gutter bx--grid--full-width">
               <div class="bx--row landing-page__tab-content">
-                <div class="bx--col-md-4 bx--col-lg-7">7/16</div>
-                <div class="bx--col-md-4 bx--offset-lg-1 bx--col-lg-8">8/16</div>
+                <div class="bx--col-md-4 bx--col-lg-7">
+                  <h2 class="landing-page__subheading">What is Carbon?</h2>
+                  <p class="landing-page__p">
+                    Carbon is IBM’s open-source design system for digital
+                    products and experiences. With the IBM Design Language as
+                    its foundation, the system consists of working code, design
+                    tools and resources, human interface guidelines, and a
+                    vibrant community of contributors.
+                  </p>
+                  <cv-button>Learn more</cv-button>
+                </div>
+                <div class="bx--col-md-4 bx--offset-lg-1 bx--col-lg-8">
+                  <img
+                    class="landing-page__illo"
+                    src="../../assets/tab-illo.png"
+                    alt="Carbon illustration"
+                  />
+                </div>
               </div>
             </div>
           </cv-tab>
+
           <cv-tab label="Design">
             <div class="bx--grid bx--grid--no-gutter bx--grid--full-width">
               <div class="bx--row landing-page__tab-content">
