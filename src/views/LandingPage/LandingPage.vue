34a9094dfb635e5725ded4951f85d41759e86d7f
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2019-08-27 11:57:40 -0500

    feat(tutorial): complete step 1

---
+++ src/views/LandingPage/LandingPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    LandingPage
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/RepoPage/RepoPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    RepoPage
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
