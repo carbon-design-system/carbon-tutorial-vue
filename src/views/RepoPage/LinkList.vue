3ef73298e08d9680bdfa13ee0a702d687bbb23be
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2019-08-28 13:58:57 -0500

    feat(tutorial): complete step 3

---
+++ src/views/RepoPage/LinkList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul class="link-list">
+      <li>
+        <cv-link :href="url">GitHub</cv-link>
+      </li>
+      <li v-if="homepageUrl">
+        <span>&nbsp;|&nbsp;</span>
+        <cv-link :href="homepageUrl">Homepage</cv-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LinkList',
+  props: {
+    url: String,
+    homepageUrl: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.link-list {
+  display: flex;
+}
+</style>
