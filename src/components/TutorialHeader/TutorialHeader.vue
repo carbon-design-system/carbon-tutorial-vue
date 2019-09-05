07cd1780ae7855c9f3abfa9de2aa034e45de5646
Ricardo Augusto <contato@ricardoaugusto.com>
2019-09-05 16:18:13 -0300

    step 1 completed

---
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <cv-header aria-label="Carbon Tutorial">
+    <cv-skip-to-content href="#main-content"
+      >Skip to content</cv-skip-to-content
+    >
+
+    <cv-header-name to="/" prefix="IBM">Carbon Tutorial</cv-header-name>
+
+    <cv-header-nav>
+      <cv-header-menu-item to="/repos">Repositories</cv-header-menu-item>
+    </cv-header-nav>
+
+    <template slot="HeaderGlobal">
+      <cv-header-global-action aria-label="Notifications">
+        <Notification20 />
+      </cv-header-global-action>
+      <cv-header-global-action aria-label="User avatar">
+        <UserAvatar20 />
+      </cv-header-global-action>
+      <cv-header-global-action aria-label="App switcher">
+        <AppSwitcher20 />
+      </cv-header-global-action>
+    </template>
+  </cv-header>
+</template>
+
+<script>
+import Notification20 from '@carbon/icons-vue/es/notification/20';
+import UserAvatar20 from '@carbon/icons-vue/es/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons-vue/es/app-switcher/20';
+
+export default {
+  name: 'TutorialHeader',
+  components: { Notification20, UserAvatar20, AppSwitcher20 }
+};
+</script>
---
+++ src/views/RepoPage/RepoPage.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Repo page</div>
+</template>
