28bff35a16b1b5a7b72f29a19eca0c589bdca169
Jasmine Herd <Jasmine.Herd@ibm.com>
2019-09-06 14:22:35 -0400

    Revert "feat(tutorial): complete step 1"
    This reverts commit a0e7c5454f868c02a6737dc848551f39c68d4fae.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,8 @@
 <template>
   <div id="app">
-    <tutorial-header />
-    <cv-content id="#main-content">
-      <router-view/>
-    </cv-content>
+    Hello Carbon! Well, not quite yet. This is the starting point for the Carbon
+    tutorial.
   </div>
 </template>
 
-<style lang="scss">
-@import "./styles/carbon";
-</style>
-
-<script>
-import TutorialHeader from "./components/TutorialHeader";
-
-export default {
-  name: "App",
-  components: {
-    TutorialHeader
-  }
-};
-</script>
+<style lang="scss"></style>
--- src/views/LandingPage/LandingPage.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>LANDING PAGE</div>
-</template>
