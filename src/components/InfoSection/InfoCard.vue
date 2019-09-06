481ef9ced0ef5f9916314d662add56bb039cad2a
Ricardo Augusto <contato@ricardoaugusto.com>
2019-09-06 14:03:45 -0300

    feat(tutorial): complete step 4

---
+++ src/components/InfoSection/InfoCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <article
+    class="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1"
+  >
+    <h4 class="info-card__heading">
+      {{ splitHeading[0] }}
+      <strong>{{ splitHeading[1] }}</strong>
+    </h4>
+    <p class="info-card__body">{{ body }}</p>
+    <component :is="icon" />
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'InfoCard',
+  props: {
+    heading: String,
+    body: String,
+    icon: Object
+  },
+  computed: {
+    splitHeading() {
+      const splitHeading = this.heading.split(' ');
+      const finalWord = splitHeading.pop();
+      return [splitHeading.join(' '), finalWord];
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import '../../styles/carbon-utils';
+
+.info-card {
+  margin-top: $spacing-09;
+  display: flex;
+  flex-direction: column;
+}
+svg {
+  margin-top: $spacing-09;
+}
+
+.info-card__heading {
+  @include carbon--type-style('productive-heading-03');
+}
+.info-card__body {
+  margin-top: $spacing-06;
+  flex-grow: 1;
+  @include type-style('body-long-01');
+  @include carbon--breakpoint-between(321px, md) {
+    max-width: 75%;
+  }
+}
+</style>
