0d381e8db49b52fce2e5864beaca7fcdd58a3c0e
Leonel Romo <sauleorom85@outlook.com>
2019-09-11 11:56:32 -0500

    feat(tutorial): complete step 3

--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,12 +1,17 @@
 <template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
+  <div v-if="loading">
+    <cv-data-table-skeleton v-if="loading" :columns="columns" :title="title" :helper-text="helperText" :rows="10" />
+  </div>
+  <cv-data-table v-else :columns="columns" :title="title" :helper-text="helperText"
+    :pagination="{ numberOfItems: this.totalRows }" @pagination="$emit('pagination', $event)">
     <template slot="data">
       <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
-        <cv-data-table-cell
-          v-for="(cell, cellIndex) in row.data"
-          :key="`${cellIndex}`"
-          >{{ cell }}</cv-data-table-cell
-        >
+        <cv-data-table-cell v-for="(cell, cellIndex) in row.data" :key="`${cellIndex}`">
+          <template v-if="!cell.url">
+            {{cell}}
+          </template>
+          <link-list v-else :url="cell.url" :homepage-url="cell.homepageUrl" />
+        </cv-data-table-cell>
         <template slot="expandedContent">{{ row.description }}</template>
       </cv-data-table-row>
     </template>
@@ -14,33 +19,38 @@
 </template>
 
 <script>
-export default {
-  name: 'RepoTable',
-  props: {
-    headers: Array,
-    rows: Array,
-    title: String,
-    helperText: String
-  },
-  computed: {
-    columns() {
-      return this.headers.map(header => header.header);
-    },
-    data() {
-      return this.rows.map(row => ({
-        data: [
-          row.name,
-          row.createdAt,
-          row.updatedAt,
-          row.issueCount,
-          row.stars,
-          row.links
-        ],
-        description: 'Row description'
-      }));
-    }
-  }
-};
-</script>
+  import LinkList from './LinkList';
 
-<style lang="scss"></style>
+  export default {
+    name: 'RepoTable',
+    components: {
+      LinkList
+    },
+    props: {
+      headers: Array,
+      rows: Array,
+      title: String,
+      helperText: String,
+      loading: Boolean,
+      totalRows: Number,
+    },
+    computed: {
+      columns() {
+        return this.headers.map(header => header.header);
+      },
+      data() {
+        return this.rows.map(row => ({
+          data: [
+            row.name,
+            row.createdAt,
+            row.updatedAt,
+            row.issueCount,
+            row.stars,
+            row.links
+          ],
+          description: row.description
+        }));
+      }
+    }
+  };
+</script>
