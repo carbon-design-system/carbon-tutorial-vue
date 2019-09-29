9d39aef04d8c7efdf978e33c32f571042e5d6717
SotaSuzuki <3.3sota.suzuki@gmail.com>
2019-09-29 10:08:49 +0900

    feat(tutorial): complete step 1

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import { LandingPage } from './views/LandingPage';
+import { RepoPage } from './views/RepoPage';
 
 Vue.use(Router);
 
@@ -8,17 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'lp',
+      component: LandingPage
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/repos',
+      name: 'repositories',
+      component: RepoPage
     }
   ]
 });
