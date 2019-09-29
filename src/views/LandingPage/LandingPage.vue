68d0ccdf8f6eac3515e7c8a3e2ee33ede95912ae
SotaSuzuki <3.3sota.suzuki@gmail.com>
2019-09-29 10:09:11 +0900

    feat(tutorial): complete step 1

---
+++ src/views/LandingPage/LandingPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Landing Page</div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage'
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/views/RepoPage/RepoPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Repo Page</div>
+</template>
+
+<script>
+export default {
+  name: 'RepoPage'
+};
+</script>
+
+<style lang="scss"></style>
