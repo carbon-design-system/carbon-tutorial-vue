baa6bc7f99dec6d0330f3d2347d9381b051b14df
Caio Bozato Aroca <caiobozatoaroca@caios-mbp-2.br.ibm.com>
2019-10-03 09:29:25 -0300

    feat(tutorial): complete step 3

---
+++ src/views/RepoPage/LinkList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul class="link-list">
+            <li>
+                <cv-link :href="url">GitHub</cv-link>
+            </li>
+
+            <li v-if="homepageUrl">
+                <span>&nbsp;|&nbsp;</span>
+                <cv-link :href="homepageUrl">Homepage</cv-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LinkList',
+  props: {
+    url: String,
+    homepageUrl: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.link-list {
+    display: flex;
+}
+</style>
