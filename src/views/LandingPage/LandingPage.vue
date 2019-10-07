6ac3603480f3726b34ec863cadc861244ce2f945
Jason Scot <jason.scot@ibm.com>
2019-10-07 16:15:53 -0400

    feat(tutorial): complete step 1

---
+++ src/views/LandingPage/LandingPage.vue
@@ -0,0 +1,14 @@
+/** * LandingPage * * */
+<template>
+  <div>LANDING PAGE</div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage'
+};
+</script>
+
+<style lang="scss" scoped>
+// @import 'LandingPage.scss';
+</style>
---
+++ src/views/RepoPage/RepoPage.vue
@@ -0,0 +1,14 @@
+/** * RepoPage * * */
+<template>
+  <div>REPO PAGE</div>
+</template>
+
+<script>
+export default {
+  name: 'RepoPage'
+};
+</script>
+
+<style lang="scss" scoped>
+// @import 'RepoPage.scss';
+</style>
