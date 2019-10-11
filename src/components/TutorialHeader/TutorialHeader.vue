ebfdae38052a7ca951b85e53a47c248986a3a463
William Glasse <william.glasse@ibm.com@Williams-MacBook-Pro.local>
2019-10-11 16:07:28 +0100

    feat(tutorial): complete step 2

--- src/components/TutorialHeader/TutorialHeader.vue
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -1,24 +1,19 @@
 <template>
   <cv-header aria-label="Carbon tutorial">
-    <cv-skip-to-content href="#main-content"
-      >Skip to content</cv-skip-to-content
-    >
-
+    <cv-skip-to-content href="#main-content">Skip to content</cv-skip-to-content>
     <cv-header-name to="/" prefix="IBM">Carbon Tutorial</cv-header-name>
-
-    <cv-header-nav aria-label="Carbon tutorial nav">
+    <cv-header-nav>
       <cv-header-menu-item to="/repos">Repositories</cv-header-menu-item>
     </cv-header-nav>
-
     <template slot="header-global">
       <cv-header-global-action aria-label="Notifications">
-        <Notification20 />
+        <notification-20 />
       </cv-header-global-action>
       <cv-header-global-action aria-label="User avatar">
-        <UserAvatar20 />
+        <user-avatar-20 />
       </cv-header-global-action>
       <cv-header-global-action aria-label="App switcher">
-        <AppSwitcher20 />
+        <app-switcher-20 />
       </cv-header-global-action>
     </template>
   </cv-header>
