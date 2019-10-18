fd65f12ae73f743808a2cbae8511f0c529cd62bc
siqiliu <siqiliu@cn.ibm.com>
2019-10-18 20:25:28 +0800

    feat(tutorial): complete step 3

--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,46 +1,55 @@
 <template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
-    <template slot="data">
-      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
-        <cv-data-table-cell
-          v-for="(cell, cellIndex) in row.data"
-          :key="`${cellIndex}`"
-          >{{ cell }}</cv-data-table-cell
-        >
-        <template slot="expandedContent">{{ row.description }}</template>
-      </cv-data-table-row>
-    </template>
-  </cv-data-table>
+    <cv-data-table-skeleton v-if="loading" :columns="columns" :title="title" :helper-text="helperText" :rows="10" />
+    <cv-data-table v-else :columns="columns" :title="title" :helper-text="helperText"
+        :pagination="{ numberOfItems: this.totalRows }" @pagination="$emit('pagination', $event)">
+        <template slot="data">
+            <cv-data-table-row v-for="row in data" :key="`${row.description}`">
+                <cv-data-table-cell v-for="cell in row.data" :key="`${cell.url}`">
+                    <template v-if="!cell.url">
+                        {{cell}}
+                    </template>
+                    <link-list v-else :url="cell.url" :homepage-url="cell.homepageUrl" />
+                </cv-data-table-cell>
+                <template slot="expandedContent">{{ row.description }}</template>
+            </cv-data-table-row>
+        </template>
+    </cv-data-table>
 </template>
 
 <script>
-export default {
-  name: 'RepoTable',
-  props: {
-    headers: Array,
-    rows: Array,
-    title: String,
-    helperText: String
-  },
-  computed: {
-    columns() {
-      return this.headers.map(header => header.header);
-    },
-    data() {
-      return this.rows.map(row => ({
-        data: [
-          row.name,
-          row.createdAt,
-          row.updatedAt,
-          row.issueCount,
-          row.stars,
-          row.links
-        ],
-        description: 'Row description'
-      }));
-    }
-  }
-};
+    import LinkList from './LinkList';
+    export default {
+        name: 'RepoTable',
+        components: {
+            LinkList
+        },
+        props: {
+            headers: Array,
+            rows: Array,
+            title: String,
+            helperText: String,
+            loading: Boolean,
+            totalRows: Number,
+        },
+        computed: {
+            columns() {
+                return this.headers.map(header => header.header);
+            },
+            data() {
+                return this.rows.map(row => ({
+                    data: [
+                        row.name,
+                        row.createdAt,
+                        row.updatedAt,
+                        row.issueCount,
+                        row.stars,
+                        row.links
+                    ],
+                    description: row.description
+                }));
+            }
+        }
+    };
 </script>
 
 <style lang="scss"></style>
