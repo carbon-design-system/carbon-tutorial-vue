ca3066742469ab037c6c88034c49bc3eec6d6b47
William Hawken <william.hawken@ibm.com>
2019-10-21 11:50:31 +0100

    feat(tutorial): complete step 1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
+<style lang="scss">
+@import './styles/carbon';
+</style>
+
 <template>
   <div id="app">
-    Hello Carbon! Well, not quite yet. This is the starting point for the Carbon
-    tutorial.
+    <tutorial-header />
+    <cv-content id="#main-content">
+      <router-view />
+    </cv-content>
   </div>
 </template>
 
 <style lang="scss"></style>
+
+<script>
+import TutorialHeader from './components/TutorialHeader';
+export default {
+  name: 'App',
+  components: {
+    TutorialHeader
+  }
+};
+</script>
