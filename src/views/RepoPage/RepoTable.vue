b8df08039b459b712dd8fcbc774583a36670bbb0
James Crawley <james.crawley@ibm.com>
2019-10-23 10:39:37 +0100

    feat(tutorial): complete step 3

--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,46 +1,52 @@
 <template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
-    <template slot="data">
-      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
-        <cv-data-table-cell
-          v-for="(cell, cellIndex) in row.data"
-          :key="`${cellIndex}`"
-          >{{ cell }}</cv-data-table-cell
-        >
-        <template slot="expandedContent">{{ row.description }}</template>
-      </cv-data-table-row>
-    </template>
-  </cv-data-table>
+    <cv-data-table-skeleton v-if="loading" :columns="columns" :title="title" :helper-text="helperText" :rows="10" />
+    <cv-data-table v-else :columns="columns" :title="title" :helper-text="helperText"
+        :pagination="{ numberOfItems: this.totalRows }" @pagination="$emit('pagination', $event)">
+        <template slot="data">
+            <cv-data-table-row v-for="row in data" :key="`${row.key}`">
+                <cv-data-table-cell v-for="(cell, cellIndex) in row.data" :key="`${cellIndex}`">
+                    <template v-if="!cell.url">{{ cell }}</template>
+                    <link-list v-else :url="cell.url" :homepage-url="cell.homepageUrl" />
+                </cv-data-table-cell>
+                <template slot="expandedContent">{{ row.description }}</template>
+            </cv-data-table-row>
+        </template>
+    </cv-data-table>
 </template>
 
 <script>
+import LinkList from './LinkList'
 export default {
-  name: 'RepoTable',
-  props: {
-    headers: Array,
-    rows: Array,
-    title: String,
-    helperText: String
-  },
-  computed: {
-    columns() {
-      return this.headers.map(header => header.header);
+    name: 'RepoTable',
+    components: {
+        LinkList
     },
-    data() {
-      return this.rows.map(row => ({
-        data: [
-          row.name,
-          row.createdAt,
-          row.updatedAt,
-          row.issueCount,
-          row.stars,
-          row.links
-        ],
-        description: 'Row description'
-      }));
+    props: {
+        headers: Array,
+        rows: Array,
+        title: String,
+        helperText: String,
+        loading: Boolean,
+        totalRows: Number
+    },
+    computed: {
+        columns() {
+            return this.headers.map(header => header.header)
+        },
+        data() {
+            return this.rows.map(row => ({
+                data: [
+                    row.name,
+                    row.createdAt,
+                    row.updatedAt,
+                    row.issueCount,
+                    row.stars,
+                    row.links
+                ],
+                description: row.description,
+                key: row.key
+            }))
+        }
     }
-  }
-};
+}
 </script>
-
-<style lang="scss"></style>
