c89b86315b460c1c3df309faf252848032c255c2
Matheus <matheusdebiasio@gmail.com>
2019-11-15 20:12:09 -0200

    feat(tutorial): complete step 1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-  <div id="app">
-    Hello Carbon! Well, not quite yet. This is the starting point for the Carbon
-    tutorial.
-  </div>
+  <cv-content id="#main-content">
+    <router-view />
+  </cv-content>
 </template>
+
+<script>
+import Notification20 from '@carbon/icons-vue/es/notification/20';
+import UserAvatar20 from '@carbon/icons-vue/es/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons-vue/es/app-switcher/20';
+export default {
+  name: 'TutorialHeader',
+  components: { Notification20, UserAvatar20, AppSwitcher20 }
+};
+</script>
 
 <style lang="scss"></style>
