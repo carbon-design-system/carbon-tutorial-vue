5d3c4b036c0b92de88af68c57eb41eeca2468d2b
Victor Kariuki <karizvic@gmail.com>
2019-12-06 22:13:07 +0300

    feat(tutorial): complete step 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import CarbonComponentsVue from '@carbon/vue';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(CarbonComponentsVue);
 
 new Vue({
   router,
---
+++ src/views/LandingPage/LandingPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>LandingPage</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
