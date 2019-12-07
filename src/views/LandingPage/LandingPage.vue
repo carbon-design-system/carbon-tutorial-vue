6af8bd9a1baff5d368411a7347fffa6bad2576a5
Matheus <48689216+matheusde@users.noreply.github.com>
2019-12-07 19:59:45 -0300

    LandingPage.vue fix

--- src/views/LandingPage/LandingPage.vue
+++ src/views/LandingPage/LandingPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bx--grid bx--grid--full-width landing-page">
     <div class="bx--row landing-page__banner">
       <div class="bx--col-lg-16">
         <cv-breadcrumb noTrailingSlash aria-label="Page navigation">
@@ -11,52 +11,54 @@
       </div>
     </div>
     <div class="bx--row landing-page__r2">
-    <div class="bx--col bx--no-gutter">
-      <cv-tabs selected="0" aria-label="Tab navigation">
-        <cv-tab label="About">
-           <div class="bx--grid bx--grid--no-gutter bx--grid--full-width">
-            <div class="bx--row landing-page__tab-content">
-              <div class="bx--col-md-4 bx--col-lg-7">
-                <h2 class="landing-page__subheading">What is Carbon?</h2>
-                <p class="landing-page__p">
-                  Carbon is IBM’s open-source design system for digital
-                  products and experiences. With the IBM Design Language as
-                  its foundation, the system consists of working code, design
-                  tools and resources, human interface guidelines, and a
-                  vibrant community of contributors.
-                </p>
-                <cv-button>Learn more</cv-button>
-              </div>
-              <div class="bx--col-md-4 bx--offset-lg-1 bx--col-lg-8">
-                <img
-                  class="landing-page__illo"
-                  src="../../assets/tab-illo.png"
-                  alt="Carbon illustration" />
-              </div>
-            </div>
-          </div>
-        </cv-tab>
-        <cv-tab label="Design">
-          <div class="bx--grid bx--grid--no-gutter bx--grid--full-width">
-            <div class="bx--row landing-page__tab-content">
-              <div class="bx--col-lg-16">
-                Rapidly build beautiful and accessible experiences. The Carbon kit
-                contains all resources you need to get started.
+      <div class="bx--col bx--no-gutter">
+        <cv-tabs selected="0" aria-label="Tab navigation">
+          <cv-tab label="About">
+            <div class="bx--grid bx--grid--no-gutter bx--grid--full-width">
+              <div class="bx--row landing-page__tab-content">
+                <div class="bx--col-md-4 bx--col-lg-7">
+                  <h2 class="landing-page__subheading">What is Carbon?</h2>
+                  <p class="landing-page__p">
+                    Carbon is IBM’s open-source design system for digital
+                    products and experiences. With the IBM Design Language as
+                    its foundation, the system consists of working code, design
+                    tools and resources, human interface guidelines, and a
+                    vibrant community of contributors.
+                  </p>
+                  <cv-button>Learn more</cv-button>
+                </div>
+                <div class="bx--col-md-4 bx--offset-lg-1 bx--col-lg-8">
+                  <img
+                    class="landing-page__illo"
+                    src="../../assets/tab-illo.png"
+                    alt="Carbon illustration"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        </cv-tab>
-        <cv-tab label="Develop">
-          <div class="bx--grid bx--grid--no-gutter bx--grid--full-width">
-            <div class="bx--row landing-page__tab-content">
-              <div class="bx--col-lg-16">
-                Carbon provides styles and components in Vanilla, Vue, Angular,
-                and Vue for anyone building on the web.
+          </cv-tab>
+          <cv-tab label="Design">
+            <div class="bx--grid bx--grid--no-gutter bx--grid--full-width">
+              <div class="bx--row landing-page__tab-content">
+                <div class="bx--col-lg-16">
+                  Rapidly build beautiful and accessible experiences. The Carbon
+                  kit contains all resources you need to get started.
+                </div>
               </div>
             </div>
-          </div>
-        </cv-tab>
-      </cv-tabs>
+          </cv-tab>
+          <cv-tab label="Develop">
+            <div class="bx--grid bx--grid--no-gutter bx--grid--full-width">
+              <div class="bx--row landing-page__tab-content">
+                <div class="bx--col-lg-16">
+                  Carbon provides styles and components in Vanilla, Vue,
+                  Angular, and Vue for anyone building on the web.
+                </div>
+              </div>
+            </div>
+          </cv-tab>
+        </cv-tabs>
+      </div>
     </div>
     <div class="bx--row landing-page__r3">
       <div class="bx--col-md-4 bx--col-lg-4">
@@ -66,7 +68,6 @@
       <div class="bx--col-md-4 bx--col-lg-4">Carbon is Modular</div>
       <div class="bx--col-md-4 bx--col-lg-4">Carbon is Consistent</div>
     </div>
-  </div>
   </div>
 </template>
 
