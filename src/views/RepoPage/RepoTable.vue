23270a0baf5a6c3086ac698631c78ae63a9e24ab
Kevin Sawicke <ksawic@ibm.com>
2019-12-19 13:07:02 -0700

    updating

--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,12 +1,33 @@
 <template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
+  <cv-data-table-skeleton
+    v-if="loading"
+    :columns="columns"
+    :title="title"
+    :helper-text="helperText"
+    :rows="10"
+  />
+  <cv-data-table
+    v-else
+    :columns="columns"
+    :title="title"
+    :helper-text="helperText"
+    :pagination="{ numberOfItems: this.totalRows }"
+    @pagination="$emit('pagination', $event)"
+    >
     <template slot="data">
-      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
+      <cv-data-table-row
+        v-for="(row, rowIndex) in data"
+        :key="row.url"
+        >
         <cv-data-table-cell
           v-for="(cell, cellIndex) in row.data"
-          :key="`${cellIndex}`"
-          >{{ cell }}</cv-data-table-cell
-        >
+          :key="cell.key"
+          >
+          <template v-if="!cell.url">
+            {{cell}}
+          </template>
+          <link-list v-else :url="cell.url" :homepage-url="cell.homepageUrl" />
+        </cv-data-table-cell>
         <template slot="expandedContent">{{ row.description }}</template>
       </cv-data-table-row>
     </template>
@@ -14,19 +35,25 @@
 </template>
 
 <script>
+import LinkList from './LinkList';
+
 export default {
   name: 'RepoTable',
   props: {
     headers: Array,
     rows: Array,
     title: String,
-    helperText: String
+    helperText: String,
+    loading: Boolean,
+    totalRows: Number
   },
   computed: {
     columns() {
       return this.headers.map(header => header.header);
     },
     data() {
+      // console.dir("BLAH")
+      // console.dir(this.rows);
       return this.rows.map(row => ({
         data: [
           row.name,
@@ -36,10 +63,11 @@ export default {
           row.stars,
           row.links
         ],
-        description: 'Row description'
+        description: row.description
       }));
     }
-  }
+  },
+  components: { LinkList },
 };
 </script>
 
