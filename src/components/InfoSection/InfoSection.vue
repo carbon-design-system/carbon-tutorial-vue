0e371ea970051cbf1b72e0dc8c6f30268b895228
weidong <wdgwg@cn.ibm.com>
2019-12-22 16:55:44 +0800

    feat(tutorial): complete step 4

---
+++ src/components/InfoSection/InfoSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="bx--row info-section">
+    <div class="bx--col-md-8 bx--col-lg-4 bx--col-xlg-3">
+      <h3 class="info-section__heading">{{ heading }}</h3>
+    </div>
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'InfoSection',
+  props: {
+    heading: String
+  }
+};
+</script>
+
+<style lang="scss">
+@import '../../styles/_carbon-utils';
+
+.info-section__heading {
+  @include carbon--type-style('heading-01');
+}
+</style>
