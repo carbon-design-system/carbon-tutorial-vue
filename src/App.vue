98241265fe415b353439c96c6330f6b8a9bfc1ee
Jen Downs <hello@jendowns.com>
2020-01-03 11:09:20 -0600

    feat: add landing and repo pages

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,22 @@
   <div id="app">
     <tutorial-header />
     <cv-content id="#main-content">
-      <cv-button>Button</cv-button>
+      <router-view />
     </cv-content>
   </div>
 </template>
 
-<style lang="scss">
-@import "./styles/carbon";
-</style>
-
 <script>
-import TutorialHeader from "./components/TutorialHeader";
+import TutorialHeader from './components/TutorialHeader';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     TutorialHeader
   }
 };
 </script>
+
+<style lang="scss">
+@import './styles/carbon';
+</style>
