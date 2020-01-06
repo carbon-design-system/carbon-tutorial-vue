6e356e3a10afb7e0912e81a88c09453860512a41
Jen Downs <hello@jendowns.com>
2020-01-06 11:08:31 -0600

    feat(pages): reorganize tags

--- src/views/RepoPage/RepoPage.vue
+++ src/views/RepoPage/RepoPage.vue
@@ -1,3 +1,18 @@
+<template>
+  <div class="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+    <div class="bx--row repo-page__r1">
+      <div class="bx--col-lg-16">
+        <repo-table
+          :headers="headers"
+          :rows="rows"
+          title="Carbon Repositories"
+          helperText="A collection of public Carbon repositories."
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
 <style lang="scss">
 @import '../../styles/carbon-utils';
 
@@ -78,18 +93,3 @@ const rows = [
   }
 ];
 </script>
-
-<template>
-  <div class="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
-    <div class="bx--row repo-page__r1">
-      <div class="bx--col-lg-16">
-        <repo-table
-          :headers="headers"
-          :rows="rows"
-          title="Carbon Repositories"
-          helperText="A collection of public Carbon repositories."
-        />
-      </div>
-    </div>
-  </div>
-</template>
--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,3 +1,18 @@
+<template>
+  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
+    <template slot="data">
+      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
+        <cv-data-table-cell
+          v-for="(cell, cellIndex) in row.data"
+          :key="`${cellIndex}`"
+          >{{ cell }}</cv-data-table-cell
+        >
+        <template slot="expandedContent">{{ row.description }}</template>
+      </cv-data-table-row>
+    </template>
+  </cv-data-table>
+</template>
+
 <script>
 export default {
   name: 'RepoTable',
@@ -27,18 +42,3 @@ export default {
   }
 };
 </script>
-
-<template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
-    <template slot="data">
-      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
-        <cv-data-table-cell
-          v-for="(cell, cellIndex) in row.data"
-          :key="`${cellIndex}`"
-          >{{ cell }}</cv-data-table-cell
-        >
-        <template slot="expandedContent">{{ row.description }}</template>
-      </cv-data-table-row>
-    </template>
-  </cv-data-table>
-</template>
