b63ac85010bc2c108cf2f8b285b2b8f30078a660
Alison Joseph <alisonejoseph@gmail.com>
2020-01-20 13:50:05 -0600

    feat(tutorial): complete step 3

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue';
 import router from './router';
 import CarbonComponentsVue from '@carbon/vue';
 import { createProvider } from './vue-apollo';
-Vue.use(CarbonComponentsVue);
 
+Vue.use(CarbonComponentsVue);
 Vue.config.productionTip = false;
 
 new Vue({
