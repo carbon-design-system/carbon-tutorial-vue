a4d47e5c053afa46142cb83b8aa97b9f1a41cb53
Alison Joseph <alisonejoseph@gmail.com>
2020-01-20 13:55:27 -0600

    feat(tutorial): complete step 3

--- src/views/RepoPage/LinkList.vue
+++ src/views/RepoPage/LinkList.vue
@@ -3,10 +3,7 @@
     <li>
       <cv-link :href="url">GitHub</cv-link>
     </li>
-<<<<<<< HEAD
 
-=======
->>>>>>> 991663f4db563c4b3ccddc1c0b509fa8989e86d7
     <li v-if="homepageUrl">
       <span>&nbsp;|&nbsp;</span>
       <cv-link :href="homepageUrl">Homepage</cv-link>
@@ -17,11 +14,6 @@
 <script>
 export default {
   name: 'LinkList',
-<<<<<<< HEAD
-=======
-  components: {},
-
->>>>>>> 991663f4db563c4b3ccddc1c0b509fa8989e86d7
   props: {
     url: String,
     homepageUrl: String
@@ -29,11 +21,7 @@ export default {
 };
 </script>
 
-<<<<<<< HEAD
 <style lang="scss">
-=======
-<style lang="scss" scoped>
->>>>>>> 991663f4db563c4b3ccddc1c0b509fa8989e86d7
 .link-list {
   display: flex;
 }
