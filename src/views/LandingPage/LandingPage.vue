65c875db193ff3ed148b9286e71bc72d3a3640e6
MAKSYM BOBIN <maksym.bobin@ua.ibm.com>
2020-02-11 18:40:30 +0200

    feat(tutorial): complete step 1

---
+++ src/views/LandingPage/LandingPage.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <div>
+    Landing Page
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css" scoped></style>
---
+++ src/views/RepoPage/RepoPage.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <div>
+    REPO PAGE
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css" scoped></style>
