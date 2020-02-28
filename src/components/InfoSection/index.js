8a3a8801cfd2ea899d08b3ff6ffed67d96daafae
fanchenyu <fancheny@cn.ibm.com>
2020-02-28 17:15:36 +0800

    feat(tutorial): complete step 4

---
+++ src/components/InfoSection/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: fanchenyu
+* @Date:   2020-02-28 16:39:02
+* @Last Modified by:   fanchenyu
+* @Last Modified time: 2020-02-28 16:40:50
+*/
+import InfoSection from './InfoSection';
+import InfoCard from './InfoCard';
+export { InfoSection, InfoCard };
