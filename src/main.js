5a2741a0efd450a07061e1abe6cffd5f716f179f
Gary <astarmist@gmail.com>
2020-04-11 16:40:28 +0800

    Remove duplicate icon and yarn.lock

--- src/main.js
+++ src/main.js
@@ -3,17 +3,6 @@ import App from './App.vue';
 import router from './router';
 import CarbonComponentsVue from "@carbon/vue";
 
-import { CarbonIconsVue } from '@carbon/icons-vue';
-import Notification20 from '@carbon/icons-vue/es/notification/20';
-import UserAvatar20 from '@carbon/icons-vue/es/user--avatar/20';
-import AppSwitcher20 from '@carbon/icons-vue/es/app-switcher/20';
-
-Vue.use(CarbonIconsVue, {
-  components: {
-    Notification20, UserAvatar20, AppSwitcher20
-  },
-});
-
 Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false;
