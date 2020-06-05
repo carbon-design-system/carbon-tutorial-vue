caa46936ef81038be8dc48d3a430a440a6c3b6d8
houyunjuan1201 <houyunjuan1201@houyunjuan1201s-MacBook-Pro.local>
2020-06-06 01:31:09 +0800

    feat(tutorial): complete step 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-
+import CarbonComponentsVue from "@carbon/vue";  
 Vue.config.productionTip = false;
+
+Vue.use(CarbonComponentsVue);
 
 new Vue({
   router,
