8a24c471b5858de0a81bc4f25552a2270949a585
RodolphoAM <rodolphoagizzio@hotmail.com>
2020-06-07 15:15:47 -0300

    finished step 3

--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,11 +1,25 @@
 <template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
+  <cv-data-table-skeleton
+    v-if="loading"
+    :columns="columns"
+    :title="title"
+    :helper-text="helperText"
+    :rows="10"
+  />
+  <cv-data-table
+  v-else
+  :columns="columns"
+  :title="title"
+  :helper-text="helperText"
+  :pagination="{ numberOfItems: this.totalRows }"
+  @pagination="$emit('pagination', $event)">
     <template slot="data">
       <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
-        <cv-data-table-cell
-          v-for="(cell, cellIndex) in row.data"
-          :key="`${cellIndex}`"
-          >{{ cell }}
+        <cv-data-table-cell v-for="(cell, cellIndex) in row.data" :key="`${cellIndex}`">
+          <template v-if="!cell.url">
+            {{cell}}
+          </template>
+          <link-list v-else :url="cell.url" :homepage-url="cell.homepageUrl" />
         </cv-data-table-cell>
         <template slot="expandedContent">
         {{ row.description }}
@@ -16,14 +30,19 @@
 </template>
 
 <script>
+import LinkList from "./LinkList";
+
 export default {
   name: 'RepoTable',
   props: {
-    headers: Array,
-    rows: Array,
-    title: String,
-    helperText: String,
-},
+      headers: Array,
+      rows: Array,
+      title: String,
+      helperText: String,
+      loading: Boolean,
+      totalRows: Number,
+  },
+  components: { LinkList },
   computed: {
     columns() {
       return this.headers.map(header => header.header);
@@ -38,7 +57,7 @@ export default {
           row.stars,
           row.links
         ],
-        description: 'Row description',
+        description: row.description,
       }));
     },
   }
