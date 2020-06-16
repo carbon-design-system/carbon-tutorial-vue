0676b73b0bd37ec6180165f9c8d68a46911651f2
QiHua Huo <huoqihua@cn.ibm.com>
2020-06-16 15:05:21 +0800

    feat(tutorial): complete step 3

--- src/views/RepoPage/RepoPage.vue
+++ src/views/RepoPage/RepoPage.vue
@@ -2,11 +2,14 @@
   <div class="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
     <div class="bx--row repo-page__r1">
       <div class="bx--col-lg-16">
-        <RepoTable
+        <repo-table
           :headers="headers"
-          :rows="rows"
           title="Carbon Repositories"
           helperText="A collection of public Carbon repositories."
+          :loading="$apollo.loading"
+          :rows="pagedRows"
+          :totalRows="rows.length"
+          @pagination="onPagination"
         />
       </div>
     </div>
@@ -15,7 +18,7 @@
 
 <script>
 import RepoTable from './RepoTable';
-
+import gql from 'graphql-tag';
 const headers = [
   {
     key: 'name',
@@ -43,44 +46,87 @@ const headers = [
   }
 ];
 
-const rows = [
-  {
-    id: '1',
-    name: 'Repo 1',
-    createdAt: 'Date',
-    updatedAt: 'Date',
-    issueCount: '123',
-    stars: '456',
-    links: 'Links'
-  },
-  {
-    id: '2',
-    name: 'Repo 2',
-    createdAt: 'Date',
-    updatedAt: 'Date',
-    issueCount: '123',
-    stars: '456',
-    links: 'Links'
-  },
-  {
-    id: '3',
-    name: 'Repo 3',
-    createdAt: 'Date',
-    updatedAt: 'Date',
-    issueCount: '123',
-    stars: '456',
-    links: 'Links'
+const REPO_QUERY = gql`
+  query REPO_QUERY {
+    # Let's use carbon as our organization
+    organization(login: "carbon-design-system") {
+      # We'll grab all the repositories in one go. To load more resources
+      # continuously, see the advanced topics.
+      repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
+        totalCount
+        nodes {
+          url
+          homepageUrl
+          issues(filterBy: { states: OPEN }) {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
+          releases(first: 1) {
+            totalCount
+            nodes {
+              name
+            }
+          }
+          name
+          updatedAt
+          createdAt
+          description
+          id
+        }
+      }
+    }
   }
-];
-
+`;
 export default {
   name: 'RepoPage',
   components: { RepoTable },
   data() {
     return {
       headers,
-      rows
+      pageSize: 0,
+      pageStart: 0,
+      page: 0
     };
+  },
+  apollo: {
+    organization: REPO_QUERY
+  },
+  computed: {
+    rows() {
+      if (!this.organization) {
+        return [];
+      } else {
+        return this.organization.repositories.nodes.map(row => ({
+          ...row,
+          key: row.id,
+          stars: row.stargazers.totalCount,
+          issueCount: row.issues.totalCount,
+          createdAt: new Date(row.createdAt).toLocaleDateString(),
+          updatedAt: new Date(row.updatedAt).toLocaleDateString(),
+          links: { url: row.url, homepageUrl: row.homepageUrl }
+        }));
+      }
+    },
+    pagedRows() {
+      return this.rows.slice(this.pageStart, this.pageStart + this.pageSize);
+    }
+  },
+  watch: {
+    rows() {
+      if (this.organization) {
+        // eslint-disable-next-line no-console
+        console.dir(this.organization.repositories.nodes);
+      }
+    }
+  },
+  methods: {
+    onPagination(val) {
+      this.pageSize = val.length;
+      this.pageStart = val.start;
+      this.page = val.page;
+    }
   }
 };
 </script>
