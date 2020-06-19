c40c2ad18f8ee6fd5d77cfeca6a4cc0998ca2830
Rodolpho Agizzio Molina <Rodolpho.Agizzio.Molina@ibm.com>
2020-06-19 14:44:30 -0300

    Query (fix)

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -5,23 +5,50 @@ import {
   restartWebsockets
 } from 'vue-cli-plugin-apollo/graphql-client';
 
-// Use our access token
+// Install the vue plugin
 Vue.use(VueApollo);
 
 // Name of the localStorage item
 const AUTH_TOKEN = process.env.VUE_APP_GITHUB_PERSONAL_ACCESS_TOKEN;
 
-// Target github api
+// Http endpoint
 const httpEndpoint =
   process.env.VUE_APP_GRAPHQL_HTTP || 'https://api.github.com/graphql';
 
 // Config
 const defaultOptions = {
-  // set wsEndpoint to null
+  // You can use `https` for secure connection (recommended in production)
+  httpEndpoint,
+  // You can use `wss` for secure connection (recommended in production)
+  // Use `null` to disable subscriptions
   wsEndpoint: process.env.VUE_APP_GRAPHQL_WS,
+  // LocalStorage token
+  tokenName: AUTH_TOKEN,
+  // Enable Automatic Query persisting with Apollo Engine
+  persisting: false,
+  // Use websockets for everything (no HTTP)
+  // You need to pass a `wsEndpoint` for this to work
+  websocketsOnly: false,
+  // Is being rendered on the server?
+  ssr: false,
 
-  // Use the form expected by github for authorisation
+  // Override default apollo link
+  // note: don't override httpLink here, specify httpLink options in the
+  // httpLinkOptions property of defaultOptions.
+  // link: myLink
+
+  // Override default cache
+  // cache: myCache
+
+  // Override the way the Authorization header is set
+  // getAuth: (tokenName) => ...
   getAuth: tokenName => `Bearer ${tokenName}`
+
+  // Additional ApolloClient options
+  // apollo: { ... }
+
+  // Client local data (see apollo-link-state)
+  // clientState: { resolvers: { ... }, defaults: { ... } }
 };
 
 // Call this in the Vue app file
