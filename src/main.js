47b01fbdaaf25ee278973440577809b76c64f09d
Bohumil Fiala <bohumil.fiala@iseco.cz>
2020-07-27 13:48:49 +0200

    feat(tutorial): complete step 1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import CarbonComponentsVue from '@carbon/vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
 
+Vue.use(CarbonComponentsVue);
 new Vue({
   router,
   render: h => h(App)
