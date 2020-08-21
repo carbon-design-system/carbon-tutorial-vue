a1db24ab0f1cb04eca08c2866afb2d0a881bfa16
David Nixon <david_nixon@us.ibm.com>
2020-08-21 14:44:12 -0400

    feat(tutorial): complete step 3

--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,27 +1,55 @@
 <template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
+  <cv-data-table-skeleton
+    v-if="loading"
+    :columns="columns"
+    :title="title"
+    :helper-text="helperText"
+    :rows="10"
+  />
+
+  <cv-data-table
+    v-else
+    :columns="columns"
+    :title="title"
+    :helper-text="helperText"
+    :pagination="{ numberOfItems: this.totalRows }"
+    @pagination="$emit('pagination', $event)"
+  >
     <template slot="data">
-      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
+      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${row.key}`">
         <cv-data-table-cell
           v-for="(cell, cellIndex) in row.data"
           :key="`${cellIndex}`"
-          >{{ cell }}</cv-data-table-cell
         >
-        <template slot="expandedContent">{{ row.description }}</template>
+          <template v-if="!cell.url">
+            {{ cell }}
+          </template>
+          <link-list v-else :url="cell.url" :homepage-url="cell.homepageUrl" />
+        </cv-data-table-cell>
+
+        <template slot="expandedContent">{{
+          row.description | replaceMarkdownEmojis
+        }}</template>
       </cv-data-table-row>
     </template>
   </cv-data-table>
 </template>
 
 <script>
+import LinkList from './LinkList';
+
 export default {
   name: 'RepoTable',
+  components: { LinkList },
   props: {
     headers: Array,
     rows: Array,
     title: String,
-    helperText: String
+    helperText: String,
+    loading: Boolean,
+    totalRows: Number
   },
+
   computed: {
     columns() {
       return this.headers.map(header => header.header);
@@ -36,8 +64,25 @@ export default {
           row.stars,
           row.links
         ],
-        description: 'Row description'
+        key: row.id,
+        description: row.description
       }));
+    }
+  },
+
+  filters: {
+    replaceMarkdownEmojis: function(val) {
+      if (val)
+        return val.replace(/:[a-z0-9_]+:/gi, function(emoji) {
+          switch (emoji) {
+            case ':chart_with_upwards_trend:':
+              return '\ud83d\udcc8';
+            case ':bar_chart:':
+              return '\ud83d\udcca';
+          }
+          return emoji;
+        });
+      else return val;
     }
   }
 };
