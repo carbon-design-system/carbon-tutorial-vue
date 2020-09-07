ca19a717a5053c7dc8a62a4d09f7e10b7073a103
Linkzero Tsang <linkzerozeng@gmail.com>
2020-09-06 22:47:54 -0700

    feat(tutorial): complete step 2

--- src/components/TutorialHeader/TutorialHeader.vue
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -1,24 +1,24 @@
 <template>
   <cv-header aria-label="Carbon tutorial">
-    <cv-skip-to-content href="#main-content"
-      >Skip to content</cv-skip-to-content
-    >
+    <cv-skip-to-content href="#main-content">
+      Skip to content
+    </cv-skip-to-content>
 
     <cv-header-name to="/" prefix="IBM">Carbon Tutorial</cv-header-name>
 
-    <cv-header-nav aria-label="Carbon tutorial nav">
+    <cv-header-nav>
       <cv-header-menu-item to="/repos">Repositories</cv-header-menu-item>
     </cv-header-nav>
 
-    <template slot="header-global">
-      <cv-header-global-action aria-label="Notifications">
-        <Notification20 />
+    <template v-slot:header-global>
+      <cv-header-global-action arial-label="Notification">
+        <notification-20 />
       </cv-header-global-action>
-      <cv-header-global-action aria-label="User avatar">
-        <UserAvatar20 />
+      <cv-header-global-action arial-label="User avatar">
+        <user-avatar-20 />
       </cv-header-global-action>
-      <cv-header-global-action aria-label="App switcher">
-        <AppSwitcher20 />
+      <cv-header-global-action arial-label="App switcher">
+        <app-switcher-20 />
       </cv-header-global-action>
     </template>
   </cv-header>
@@ -31,8 +31,12 @@ import AppSwitcher20 from '@carbon/icons-vue/es/app-switcher/20';
 
 export default {
   name: 'TutorialHeader',
-  components: { Notification20, UserAvatar20, AppSwitcher20 }
+  components: {
+    Notification20,
+    UserAvatar20,
+    AppSwitcher20
+  }
 };
 </script>
 
-<style lang="scss"></style>
+<style></style>
