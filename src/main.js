210645b24933dfaf92ca7482cb4439b407a81623
Iulia Cristina Hatiegan <Iulia.Hatiegan@ibm.com>
2020-10-20 12:44:38 +0200

    feat(tutorial): complete step 1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import CarbonComponentsVue from "@carbon/vue";
+Vue.use(CarbonComponentsVue);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount("#app");
--- src/router.js
+++ src/router.js
@@ -1,24 +1,24 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import LandingPage from "./views/LandingPage";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "landing-page",
+      component: LandingPage,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/repos",
+      name: "repo-page",
       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
+      // this generates a separate chunk (repo-page.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+        import(/* webpackChunkName: "repo-page" */ "./views/RepoPage"),
+    },
+  ],
 });
