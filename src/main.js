661ff0cfce17474fe5c7c9ffc6ad2f20e4d5b5c5
Daniel Moreto <danifim@br.ibm.com>
2020-11-17 11:44:51 -0300

    feat(tutorial): complete step 3

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import CarbonComponentsVue from '@carbon/vue';
+import { createProvider } from './vue-apollo';
+
 Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app');
