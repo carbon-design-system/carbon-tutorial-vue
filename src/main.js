7481a9e4e8360b38d23742671f7b23d59de0e9be
Aalasa Fidler <Aalasa.Fidler@ibm.com>
2020-11-18 12:11:48 +0000

    feat(tutorial): complete step 1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// This is a quick way to pull in all @carbon/vue components and register them for use in your project.
+import CarbonComponentsVue from '@carbon/vue';
+Vue.use(CarbonComponentsVue);
+
 Vue.config.productionTip = false;
 
 new Vue({
