0f3d25dc2311292aeed1a8309e0c602d3ec98177
xuwenbin2020 <xuwenb@cn.ibm.com>
2020-11-25 21:42:15 +0800

    feat(tutorial): complete step 2

--- src/views/RepoPage/RepoPage.vue
+++ src/views/RepoPage/RepoPage.vue
@@ -1,3 +1,102 @@
 <template>
-  <div>REPO PAGE</div>
+  <div class="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+    <div class="bx--row repo-page__r1">
+      <div class="bx--col-lg-16">
+		  <repo-table
+		    :headers="headers"
+		    :rows="rows"
+		    title="Carbon Repositories"
+		    helperText="A collection of public Carbon repositories."
+		  />
+	  </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import RepoTable from "./RepoTable";
+
+const headers = [
+  {
+    key: 'name',
+    header: 'Name',
+  },
+  {
+    key: 'createdAt',
+    header: 'Created',
+  },
+  {
+    key: 'updatedAt',
+    header: 'Updated',
+  },
+  {
+    key: 'issueCount',
+    header: 'Open Issues',
+  },
+  {
+    key: 'stars',
+    header: 'Stars',
+  },
+  {
+    key: 'links',
+    header: 'Links',
+  },
+];
+
+const rows = [
+  {
+    id: '1',
+    name: 'Repo 1',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links',
+  },
+  {
+    id: '2',
+    name: 'Repo 2',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links',
+  },
+  {
+    id: '3',
+    name: 'Repo 3',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links',
+  },
+];
+  export default {
+    name: 'RepoTable',
+    props: {
+      headers: Array,
+      rows: Array,
+      title: String,
+      helperText: String,
+    },
+    computed: {
+      columns() {
+        return this.headers.map(header => header.header);
+      },
+      data() {
+        return this.rows.map(row => ({
+          data: [
+            row.name,
+            row.createdAt,
+            row.updatedAt,
+            row.issueCount,
+            row.stars,
+            row.links,
+          ],
+          description: 'Row description',
+        }));
+      },
+    },
+  };
+</script>
---
+++ src/views/RepoPage/RepoTable.vue
@@ -0,0 +1,26 @@
+<template>
+  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
+	  <template slot="data">
+		<cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`"> 
+			<cv-data-table-cell
+			  v-for="(cell, cellIndex) in row.data"
+			  :key="`${cellIndex}`"
+			  >{{cell}}</cv-data-table-cell>
+			  
+			  <template slot="expandedContent">
+			  {{ row.description }}
+			  </template>
+		</cv-data-table-row>
+	  </template>
+  </cv-data-table>
+</template>
+
+
+<style lang="scss">
+@import '../../styles/carbon-utils';
+
+.repo-page .bx--row {
+  padding-top: $spacing-05;
+  padding-bottom: $spacing-05;
+}
+</style>
