3d523739cc7ecf66ad74b2d346a4ce9cb1040c3b
xuwenbin2020 <xuwenb@cn.ibm.com>
2020-11-25 22:27:07 +0800

    feat(tutorial): complete step 3

--- src/views/RepoPage/RepoPage.vue
+++ src/views/RepoPage/RepoPage.vue
@@ -2,11 +2,13 @@
   <div class="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
     <div class="bx--row repo-page__r1">
       <div class="bx--col-lg-16">
-        <RepoTable
+		{{ this.organization }}  
+        <repo-table
           :headers="headers"
           :rows="rows"
           title="Carbon Repositories"
           helperText="A collection of public Carbon repositories."
+          :loading="$apollo.loading"
         />
       </div>
     </div>
@@ -15,7 +17,40 @@
 
 <script>
 import RepoTable from './RepoTable';
-
+import gql from 'graphql-tag';
+const REPO_QUERY = gql`
+  query REPO_QUERY {
+    # Let's use carbon as our organization
+    organization(login: "carbon-design-system") {
+      # We'll grab all the repositories in one go. To load more resources
+      # continuously, see the advanced topics.
+      repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
+        totalCount
+        nodes {
+          url
+          homepageUrl
+          issues(filterBy: { states: OPEN }) {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
+          releases(first: 1) {
+            totalCount
+            nodes {
+              name
+            }
+          }
+          name
+          updatedAt
+          createdAt
+          description
+          id
+        }
+      }
+    }
+  }
+`;
 const headers = [
   {
     key: 'name',
@@ -76,12 +111,35 @@ const rows = [
 export default {
   name: 'RepoPage',
   components: { RepoTable },
-  data() {
-    return {
-      headers,
-      rows
-    };
-  }
+	data() {
+	  return {
+		headers,
+		pageSize: 0,
+		pageStart: 0,
+		page: 0
+	  };
+	},
+    computed: {
+      // other computed properties
+      // ...
+      pagedRows() {
+        return this.rows.slice(this.pageStart, this.pageStart + this.pageSize);
+      }
+    },
+    methods: {
+      onPagination(val) {
+        this.pageSize = val.length;
+        this.pageStart = val.start;
+        this.page = val.page;
+      }
+    },
+	  watch: {
+	    rows() {
+	      if (this.organization) {
+	        console.dir(this.organization.repositories.nodes);
+	      }
+	    }
+	  }
 };
 </script>
 
