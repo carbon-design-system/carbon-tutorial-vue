7ad559d8588a583dafcf63b6e6534001d6d99d75
xucongd@cn.ibm.com <xucongd@cn.ibm.com>
2020-11-26 13:22:51 +0800

    feat(tutorial): complete step 2

--- src/views/LandingPage/LandingPage.vue
+++ src/views/LandingPage/LandingPage.vue
@@ -19,7 +19,7 @@
 		      <div class="bx--col-md-4 bx--col-lg-7">
 			<h2 class="landing-page__subheading">What is Carbon?</h2>
 			<p class="landing-page__p">
-			  Carbon is IBM¡¯s open-source design system for digital
+			  Carbon is IBM's open-source design system for digital
 			  products and experiences. With the IBM Design Language as
 			  its foundation, the system consists of working code, design
 			  tools and resources, human interface guidelines, and a
