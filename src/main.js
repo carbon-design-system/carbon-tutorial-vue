d82d3d21702afc77627960eefd39181278fd649d
Qi Xi <xiqish@cn.ibm.com>
2020-12-07 00:28:54 +0800

    feat(tutorial): complete step 1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import CarbonComponentsVue from '@carbon/vue';
+Vue.use(CarbonComponentsVue);
+
 Vue.config.productionTip = false;
 
 new Vue({
