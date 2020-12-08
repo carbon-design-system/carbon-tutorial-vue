fae1dbff40dcb10f1b15b7c93228263fb50fd0ab
Fabio Lorenzi <fabio.lorenzi1@ibm.com>
2020-12-08 11:15:56 +0000

    feat(tutorial): complete step 1

---
+++ src/views/LandingPage/LandingPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Landing Page</div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage'
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/views/RepoPage/RepoPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Repo Page</div>
+</template>
+
+<script>
+export default {
+  name: 'RepoPage'
+};
+</script>
+
+<style lang="scss">
+</style>
