f10322a1fb99fa336d8dfb57674d57c3d12d1e4c
Giovana do Nascimento Pena <giovana.np1@gmail.com>
2020-12-17 17:50:48 -0300

    feat(tutorial): complete step 1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import CarbonComponentsVue from '@carbon/vue';
 
 Vue.config.productionTip = false;
+Vue.use(CarbonComponentsVue);
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app');
