ea4c52b2935bcc1e0620eb879ef5b822b8f3c935
Caio Armestrong Fernandes Costa Jardim <caio.a@ibm.com>
2020-12-22 18:01:51 -0300

    feat(tutorial): complete step 1

---
+++ src/views/LandingPage/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>LANDING PAGE</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/RepoPage/RepoPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>REPO PAGE</div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
