ecebb47d298b8a950185cf3a5fdb21335e5a718e
Mohamed Eshaftri <mohamed.eshaftri@ibm.com>
2021-01-08 15:49:24 +0100

    feat(tutorial): complete step 3

--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,27 +1,55 @@
 <template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
-    <template slot="data">
-      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
-        <cv-data-table-cell
-          v-for="(cell, cellIndex) in row.data"
-          :key="`${cellIndex}`"
-          >{{ cell }}</cv-data-table-cell
-        >
-        <template slot="expandedContent">{{ row.description }}</template>
-      </cv-data-table-row>
-    </template>
-  </cv-data-table>
+  <div>
+    <cv-data-table-skeleton
+      v-if="loading"
+      :columns="columns"
+      :title="title"
+      :helper-text="helperText"
+      :rows="10"
+    />
+    <cv-data-table
+      v-else
+      :columns="columns"
+      :title="title"
+      :helper-text="helperText"
+      :pagination="{ numberOfItems: this.totalRows }"
+      @pagination="$emit('pagination', $event)"
+    >
+      <template slot="data">
+        <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
+          <cv-data-table-cell
+            v-for="(cell, cellIndex) in row.data"
+            :key="`${cellIndex}`"
+          >
+            <template v-if="!cell.url">
+              {{ cell }}
+            </template>
+            <link-list
+              v-else
+              :url="cell.url"
+              :home-url="cell.homepageURL"
+            ></link-list>
+          </cv-data-table-cell>
+          <template slot="expandedContent">{{ row.description }}</template>
+        </cv-data-table-row>
+      </template>
+    </cv-data-table>
+  </div>
 </template>
 
 <script>
+import LinkList from './LinkList';
 export default {
   name: 'RepoTable',
   props: {
     headers: Array,
     rows: Array,
     title: String,
-    helperText: String
+    helperText: String,
+    loading: Boolean,
+    totalRows: Number
   },
+  components: { LinkList },
   computed: {
     columns() {
       return this.headers.map(header => header.header);
@@ -32,11 +60,11 @@ export default {
           row.name,
           row.createdAt,
           row.updatedAt,
-          row.issueCount,
+          row.issuesCount,
           row.stars,
           row.links
         ],
-        description: 'Row description'
+        description: row.description
       }));
     }
   }
