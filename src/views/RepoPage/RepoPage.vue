ab4c615533560026c2329f4a1f601507a2fc9f8f
fengrj <fengrj@cn.ibm.com>
2021-01-26 11:06:50 +0800

    feat(tutorial): complete step 2

--- src/views/RepoPage/RepoPage.vue
+++ src/views/RepoPage/RepoPage.vue
@@ -1,3 +1,90 @@
 <template>
-  <div>REPO PAGE</div>
+  <div>
+    <repo-table
+      :headers="headers"
+      :rows="rows"
+      title="Carbon Repositories"
+      helperText="A collection of public Carbon repositories."
+    />
+  </div>
 </template>
+<script>
+import RepoTable from './RepoTable';
+
+const headers = [
+  {
+    key: 'name',
+    header: 'Name'
+  },
+  {
+    key: 'createdAt',
+    header: 'Created'
+  },
+  {
+    key: 'updatedAt',
+    header: 'Updated'
+  },
+  {
+    key: 'issueCount',
+    header: 'Open Issues'
+  },
+  {
+    key: 'stars',
+    header: 'Stars'
+  },
+  {
+    key: 'links',
+    header: 'Links'
+  }
+];
+
+const rows = [
+  {
+    id: '1',
+    name: 'Repo 1',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links'
+  },
+  {
+    id: '2',
+    name: 'Repo 2',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links'
+  },
+  {
+    id: '3',
+    name: 'Repo 3',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links'
+  }
+];
+
+export default {
+  name: 'RepoPage',
+  components: { RepoTable },
+  data() {
+    return {
+      headers,
+      rows
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import '../../styles/carbon-utils';
+
+.repo-page .bx--row {
+  padding-top: $spacing-05;
+  padding-bottom: $spacing-05;
+}
+</style>
