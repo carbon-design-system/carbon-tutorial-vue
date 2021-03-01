7185fcc55cfba530e7b96b78cb64c682b63f5117
Lee Chase <lee.chase@uk.ibm.com>
2021-03-01 17:41:06 +0000

    feat: add basic test setup

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,34 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import App from '@/App.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('App', () => {
+  const dummyContent = 'dummy content';
+
+  it('should load app with dummy content', () => {
+    const wrapper = shallowMount(App, {
+      slots: { default: dummyContent }
     });
-    expect(wrapper.text()).toMatch(msg);
+    const app = wrapper.find('#app');
+    expect(app).not.toBeNull;
   });
 });
+
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message';
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     });
+//     expect(wrapper.text()).toMatch(msg);
+//   });
+// });
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
