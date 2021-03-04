9c70c10fc6000e3a5e1a854a4b4b9b059d3ebe9d
camposvi <viniciuscampos4@gmail.com>
2021-03-04 01:23:39 -0300

    feat(tutorial): complete step 1

--- src/components/TutorialHeader/index.js
+++ src/components/TutorialHeader/index.js
@@ -1,6 +1,2 @@
 import TutorialHeader from './TutorialHeader';
-<<<<<<< HEAD
 export default TutorialHeader;
-=======
-export default TutorialHeader;
->>>>>>> d3e50f541665874bb4d8b1f6c546372115999d17
