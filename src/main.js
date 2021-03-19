bf03911e79104fe7cf895f128e9ec9721e1afa1f
Mateus Devino <mdevino@ibm.com>
2021-03-19 12:37:47 -0300

    Adding header

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import CarbonComponentVue from '@carbon/vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(CarbonComponentVue);
 
 new Vue({
   router,
