3e69c3f86202f5a1e9bf3fbe95d019bbbe7b808d
Mateus Devino <mdevino@ibm.com>
2021-03-19 12:56:15 -0300

    Step 1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <tutorial-header />
     <cv-content id="#main-content">
-      <cv-button>Button</cv-button>
+      <router-view />
     </cv-content>
   </div>
 </template>
@@ -13,8 +13,8 @@ import TutorialHeader from './components/TutorialHeader';
 export default {
   name: 'App',
   components: {
-    TutorialHeader,
-  },
+    TutorialHeader
+  }
 };
 </script>
 
--- src/components/TutorialHeader/TutorialHeader.vue
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -4,22 +4,22 @@
       >Skip to content</cv-skip-to-content
     >
 
-    <cv-header-name href="/" prefix="IBM">Carbon Tutorial</cv-header-name>
+    <cv-header-name to="/" prefix="IBM">Carbon Tutorial</cv-header-name>
 
     <cv-header-nav>
-      <cv-header-menu-item href="/repos">Repositories</cv-header-menu-item>
+      <cv-header-menu-item to="/repos">Repositories</cv-header-menu-item>
     </cv-header-nav>
 
     <template slot="header-global">
-        <cv-header-global-action aria-label="Notifications">
-            <notification-20 />
-        </cv-header-global-action>
-        <cv-header-global-action aria-label="User avatar">
-            <user-avatar-20 />
-        </cv-header-global-action>
-        <cv-header-global-action aria-label="App switcher">
-            <app-switcher-20 />
-        </cv-header-global-action>
+      <cv-header-global-action aria-label="Notifications">
+        <notification-20 />
+      </cv-header-global-action>
+      <cv-header-global-action aria-label="User avatar">
+        <user-avatar-20 />
+      </cv-header-global-action>
+      <cv-header-global-action aria-label="App switcher">
+        <app-switcher-20 />
+      </cv-header-global-action>
     </template>
   </cv-header>
 </template>
@@ -31,6 +31,6 @@ import AppSwitcher20 from '@carbon/icons-vue/es/app-switcher/20';
 
 export default {
   name: 'TutorialHeader',
-  components: { Notification20, UserAvatar20, AppSwitcher20 },
+  components: { Notification20, UserAvatar20, AppSwitcher20 }
 };
 </script>
