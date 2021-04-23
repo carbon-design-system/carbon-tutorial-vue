b8202a0bfda6e1daa8c484062dc979b04dd781f4
Lee Chase <lee.chase@uk.ibm.com>
2021-04-23 16:01:34 +0100

    feat: add test rig

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,10 +1,8 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount as shallow } from '@vue/test-utils';
 import App from '@/App.vue';
 
 describe('App', () => {
-  it('should load app with dummy content', () => {
-    const wrapper = shallowMount(App);
-    const app = wrapper.find('#app');
-    expect(app).not.toBeNull;
+  it('renders without crashing', () => {
+    shallow(App);
   });
 });
