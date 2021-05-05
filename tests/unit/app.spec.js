4aaa17c892897a952fb42b85755740f4c68f6b69
Lee Chase <lee.chase@uk.ibm.com>
2021-05-05 14:16:34 +0100

    feat: step 2 tests

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,5 +1,7 @@
 import { RouterLinkStub, shallowMount, mount } from '@vue/test-utils';
 import App from '@/App.vue';
+import RepoPage from '../../src/views/RepoPage/RepoPage.vue';
+import LandingPage from '../../src/views/LandingPage/LandingPage.vue';
 import RouterViewStub from '../stubs/router-view-stub.vue';
 
 describe('Vue Step 1 Tests', () => {
@@ -12,18 +14,18 @@ describe('Vue Step 1 Tests', () => {
     });
   });
 
-  it('It has a Carbon header', () => {
-    const wrapper = mount(App, {
-      stubs: {
-        RouterLink: RouterLinkStub,
-        'router-view': RouterViewStub
-      }
-    });
+  it('It has a LandingPage page with tabs, landing page content and principles on it', () => {
+    const wrapper = mount(LandingPage);
 
-    wrapper.get('.bx--header');
-    wrapper.get('.bx--content');
-    wrapper.get('.bx--skip-to-content');
-    wrapper.get('.bx--header__name');
-    wrapper.get('.bx--header__nav');
+    wrapper.get('.landing-page__r2 [role="tablist"]');
+    wrapper.get('.landing-page__r2 .landing-page__tab-content');
+    wrapper.get('.landing-page__r3 .landing-page__label');
+  });
+
+  it('It has a RepoPage with a table in it', () => {
+    const wrapper = mount(RepoPage);
+
+    // Has the user rendered a table
+    wrapper.get('table');
   });
 });
