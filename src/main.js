685fc46ecc43b896a889b6edaa0714570edf6cc9
Alexander Six <alexanderhsix@gmail.com>
2021-05-16 11:38:28 -0400

    feat(tutorial): complete step 1
    Add Carbon styles to project
    
    Add Carbon button to App icomponent
    
    Add component content to TutorialHeader
    
    Import and use Carbon iconography
    
    Add Tutorial Header content
    
    Add Landing and Repo page Vue component skeletons
    
    Update Vue router to use new page components
    
    Update header nav menu items to use Vue Router
    
    Remove unused vue-cli-generated pages

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router';
 
 Vue.config.productionTip = false;
 
+/**
+ * Import Carbon components
+ */
+import CarbonComponentsVue from '@carbon/vue';
+Vue.use(CarbonComponentsVue);
+
 new Vue({
   router,
   render: h => h(App)
