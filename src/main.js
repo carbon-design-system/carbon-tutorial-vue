a522e66ceb773277f7f5c2c30df17dacdc1fa8fb
phsoares <phsoares@github.ibm.com>
2021-05-26 20:45:37 -0300

    feat(tutorial): complete step 3

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import CarbonComponentsVue from '@carbon/vue';
 Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false;
+import { createProvider } from './vue-apollo';
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app');
