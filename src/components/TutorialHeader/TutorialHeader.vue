8ddef5bebf1ba93bf6efe70ad56486096d8f4f96
Lee Chase <leechase@live.com>
2021-05-26 13:39:22 +0100

    Update TutorialHeader.vue
    Try non /es import

--- src/components/TutorialHeader/TutorialHeader.vue
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -23,9 +23,7 @@
 </template>
 
 <script>
-import Notification20 from '@carbon/icons-vue/es/notification/20';
-import UserAvatar20 from '@carbon/icons-vue/es/user--avatar/20';
-import AppSwitcher20 from '@carbon/icons-vue/es/app-switcher/20';
+import { Notification20, UserAvatar20, AppSwitcher20} from '@carbon/icons-vue';
 
 export default {
   name: 'TutorialHeader',
