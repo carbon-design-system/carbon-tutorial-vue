9cabcb3a131305a07daa031fb081b5c5386a8914
Douglas Diez Goncalves <dougdiez@gmail.com>
2021-06-01 22:00:25 -0300

    feat(tutorial): complete step 1

--- src/components/TutorialHeader/TutorialHeader.vue
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -9,23 +9,21 @@
     </cv-header-nav>
 
     <template slot="header-global">
-        <cv-header-global-action aria-label="Notifications">
-            <notification-20 />
-        </cv-header-global-action>
-        <cv-header-global-action aria-label="User avatar">
-            <user-avatar-20 />
-        </cv-header-global-action>
-        <cv-header-global-action aria-label="App switcher">
-            <app-switcher-20 />
-        </cv-header-global-action>
-
+      <cv-header-global-action aria-label="Notifications"><notification-20 />
+      </cv-header-global-action>
+      <cv-header-global-action aria-label="User avatar"><user-avatar-20 />
+      </cv-header-global-action>
+      <cv-header-global-action aria-label="App switcher"><app-switcher-20 />
+      </cv-header-global-action>
+    </template>
+  </cv-header>
 </template>
 
 <script>
 import { Notification20, UserAvatar20, AppSwitcher20 } from '@carbon/icons-vue';
 
 export default {
-  name: "TutorialHeader",
+  name: 'TutorialHeader',
   components: { Notification20, UserAvatar20, AppSwitcher20 }
 };
 </script>
