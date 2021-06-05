607ebedf3ee34f80f98193aeaa9ee102df89c948
Fabiano Duarte <fabianoad@ibm.com>
2021-06-05 12:46:41 -0300

    apollo added

---
+++ src/views/RepoPage/LinkList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="link-list">
+    <li>
+      <cv-link :href="url">GitHub</cv-link>
+    </li>
+    <li v-if="homepageUrl">
+      <span>&nbsp;|&nbsp;</span>
+      <cv-link :href="homepageUrl">Homepage</cv-link>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'LinkList',
+  props: {
+    url: String,
+    homepageUrl: String
+  }
+};
+</script>
+<style lang="scss" scoped>
+.link-list {
+  display: flex;
+}
+</style>
