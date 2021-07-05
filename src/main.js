7ebeb969d2d49f4ee1bf2fcf8ada4260fa5df26b
Ejez <ejez@protonmail.ch>
2021-07-05 21:45:05 +0300

    feat(tutorial): complete step 1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+import CarbonComponentsVue from '@carbon/vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+
+Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false;
 
