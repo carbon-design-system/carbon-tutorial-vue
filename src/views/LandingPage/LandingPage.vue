321d54105fe3dc14daff238912d7fdb215d321bf
Ghislain Bugingo <ghislainbugingo@Ghislains-MacBook-Pro.local>
2021-07-22 15:14:59 -0700

    feat(tutorial): complete step 2 v-2

--- src/views/LandingPage/LandingPage.vue
+++ src/views/LandingPage/LandingPage.vue
@@ -61,12 +61,13 @@
       </div>
     </div>
     <div class="bx--row landing-page__r3">
-      <div class="bx--col-md-4 bx--col-lg-4">The Principles</div>
+      <div class="bx--col-md-4 bx--col-lg-4">
+        <h3 class="landing-page__label">The Principles</h3>
+      </div>
       <div class="bx--col-md-4 bx--col-lg-4">Carbon is Open</div>
       <div class="bx--col-md-4 bx--col-lg-4">Carbon is Modular</div>
       <div class="bx--col-md-4 bx--col-lg-4">Carbon is Consistent</div>
     </div>
-  </div>
 </template>
 
 <style lang="scss">
