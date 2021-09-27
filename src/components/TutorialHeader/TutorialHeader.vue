8578668761823e77594feeb76defb1fd52e8eb7f
Giovana do Nascimento Pena <giovanapena@MacBook-Pro-de-Giovana.local>
2021-09-27 14:27:21 -0300

    feat(tutorial): complete step 1

---
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <cv-header aria-label="Carbon tutorial">
+    <cv-skip-to-content href="#main-content"
+      >Skip to content</cv-skip-to-content
+    >
+
+    <cv-header-name to="/" prefix="IBM">Carbon Tutorial</cv-header-name>
+
+    <cv-header-nav>
+      <cv-header-menu-item to="/repos">Repositories</cv-header-menu-item>
+    </cv-header-nav>
+
+    <template slot="header-global">
+      <cv-header-global-action aria-label="Notifications">
+        <notification-20 />
+      </cv-header-global-action>
+      <cv-header-global-action aria-label="User avatar">
+        <user-avatar-20 />
+      </cv-header-global-action>
+      <cv-header-global-action aria-label="App switcher">
+        <app-switcher-20 />
+      </cv-header-global-action>
+    </template>
+  </cv-header>
+</template>
+
+<script>
+import { Notification20, UserAvatar20, AppSwitcher20 } from '@carbon/icons-vue';
+
+export default {
+  name: 'TutorialHeader',
+  components: { Notification20, UserAvatar20, AppSwitcher20 }
+};
+</script>
