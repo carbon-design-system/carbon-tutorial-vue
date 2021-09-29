978600f028d92c081f121504a66edc1f7292c37f
camposvi <viniciuscampos4@gmail.com>
2021-09-29 11:50:54 -0300

    adding changes

--- src/components/TutorialHeader/index.js
+++ src/components/TutorialHeader/index.js
@@ -1,6 +1,2 @@
 import TutorialHeader from './TutorialHeader';
-<<<<<<< HEAD
-=======
-export { TutorialHeader };
->>>>>>> c1001190dd4817bd65f746f7ccc325af6fa3c589
 export default TutorialHeader;
--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-<<<<<<< HEAD
 
 import CarbonComponentsVue from '@carbon/vue';
 import { createProvider } from './vue-apollo';
-=======
-import CarbonComponentsVue from '@carbon/vue';
-import { createProvider } from './vue-apollo'
->>>>>>> c1001190dd4817bd65f746f7ccc325af6fa3c589
 Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false;
