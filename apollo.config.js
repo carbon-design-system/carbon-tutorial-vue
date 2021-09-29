d5ba06dce258c388434b7ee23f26dbde70d5ffe0
camposvi <viniciuscampos4@gmail.com>
2021-09-29 13:18:04 -0300

    adding changes

--- apollo.config.js
+++ apollo.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path');
 
 // Load .env files
@@ -7,16 +6,6 @@ const env = loadEnv([
   path.resolve(__dirname, '.env'),
   path.resolve(__dirname, '.env.local')
 ]);
-=======
-const path = require('path')
-
-// Load .env files
-const { loadEnv } = require('vue-cli-plugin-apollo/utils/load-env')
-const env = loadEnv([
-  path.resolve(__dirname, '.env'),
-  path.resolve(__dirname, '.env.local')
-])
->>>>>>> c1001190dd4817bd65f746f7ccc325af6fa3c589
 
 module.exports = {
   client: {
@@ -25,21 +14,13 @@ module.exports = {
   },
   service: {
     name: env.VUE_APP_APOLLO_ENGINE_SERVICE,
-<<<<<<< HEAD
     localSchemaFile: path.resolve(
       __dirname,
       './node_modules/.temp/graphql/schema.json'
     )
-=======
-    localSchemaFile: path.resolve(__dirname, './node_modules/.temp/graphql/schema.json')
->>>>>>> c1001190dd4817bd65f746f7ccc325af6fa3c589
   },
   engine: {
     endpoint: process.env.APOLLO_ENGINE_API_ENDPOINT,
     apiKey: env.VUE_APP_APOLLO_ENGINE_KEY
   }
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> c1001190dd4817bd65f746f7ccc325af6fa3c589
