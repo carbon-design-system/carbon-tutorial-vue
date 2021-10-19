5a65bf63fde63721948c49d4fd579096f42b2b3b
DQ <daiqid@cn.ibm.com>
2021-10-19 15:07:14 +0800

    fix diff

--- tests/setup.js
+++ tests/setup.js
@@ -1,17 +1,7 @@
 import Vue from 'vue';
 
-<<<<<<< HEAD
 import CarbonComponentsVue from '@carbon/vue';
 Vue.use(CarbonComponentsVue);
-=======
-<<<<<<< HEAD
-import CarbonComponentsVue from '@carbon/vue';
-Vue.use(CarbonComponentsVue);
-=======
-// import CarbonComponentsVue from '@carbon/vue';
-// Vue.use(CarbonComponentsVue);
->>>>>>> master
->>>>>>> vue-step-1
 
 // import VueRouter from "vue-router";
 // Vue.use(VueRouter);
--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { RouterLinkStub, shallowMount, mount } from '@vue/test-utils';
 import App from '@/App.vue';
 import RepoPage from '../../src/views/RepoPage/RepoPage.vue';
@@ -6,14 +5,6 @@ import LandingPage from '../../src/views/LandingPage/LandingPage.vue';
 import RouterViewStub from '../stubs/router-view-stub.vue';
 
 describe('Vue Step 2 Tests', () => {
-=======
-<<<<<<< HEAD
-import { RouterLinkStub, shallowMount, mount } from '@vue/test-utils';
-import App from '@/App.vue';
-import RouterViewStub from '../stubs/router-view-stub.vue';
-
-describe('Vue Step 1 Tests', () => {
->>>>>>> vue-step-1
   it('Renders without crashing', () => {
     const wrapper = shallowMount(App, {
       stubs: {
@@ -23,7 +14,6 @@ describe('Vue Step 1 Tests', () => {
     });
   });
 
-<<<<<<< HEAD
   it('It has a LandingPage page with tabs, landing page content and principles on it', () => {
     const wrapper = mount(LandingPage);
 
@@ -37,30 +27,5 @@ describe('Vue Step 1 Tests', () => {
 
     // Has the user rendered a table
     wrapper.get('table');
-=======
-  it('It has a Carbon header', () => {
-    const wrapper = mount(App, {
-      stubs: {
-        RouterLink: RouterLinkStub,
-        'router-view': RouterViewStub
-      }
-    });
-
-    wrapper.get('.bx--header');
-    wrapper.get('.bx--content');
-    wrapper.get('.bx--skip-to-content');
-    wrapper.get('.bx--header__name');
-    wrapper.get('.bx--header__nav');
-=======
-import { shallowMount } from '@vue/test-utils';
-import App from '@/App.vue';
-
-describe('App', () => {
-  it('should load app with dummy content', () => {
-    const wrapper = shallowMount(App);
-    const app = wrapper.find('#app');
-    expect(app).not.toBeNull;
->>>>>>> master
->>>>>>> vue-step-1
   });
 });
