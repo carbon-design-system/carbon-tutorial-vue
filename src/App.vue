af2707b7935c91c7c3b07e46048fa8d685aeff64
DQ <daiqid@cn.ibm.com>
2021-10-19 12:25:26 +0800

    fix code style

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,18 @@
   <div id="app">
     <tutorial-header />
 
-      <cv-content id="#main-content">
-  <router-view />
-
-</cv-content>
+    <cv-content id="#main-content">
+      <router-view />
+    </cv-content>
   </div>
-
 </template>
 
 <script>
-import TutorialHeader from "./components/TutorialHeader";
+import TutorialHeader from './components/TutorialHeader';
 export default {
-  name: "App",
+  name: 'App',
   components: {
     TutorialHeader
   }
 };
 </script>
-
-
-
-
-
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import CarbonComponentsVue from "@carbon/vue";
+import CarbonComponentsVue from '@carbon/vue';
 
 Vue.config.productionTip = false;
 Vue.use(CarbonComponentsVue);
@@ -10,4 +10,3 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app');
-
