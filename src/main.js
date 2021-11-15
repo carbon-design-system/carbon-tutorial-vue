bbe688a8321beb8a8068bf840a4a710030a14b98
Eliezer Rangel <eliezer.rangel@escala24x7.com>
2021-11-14 23:06:14 -0500

    feat(tutorial): complete step 3

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue';
 import router from './router';
 
 import CarbonComponentsVue from '@carbon/vue';
-Vue.use(CarbonComponentsVue);
+import { createProvider } from './vue-apollo';
 
+Vue.use(CarbonComponentsVue);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app');
