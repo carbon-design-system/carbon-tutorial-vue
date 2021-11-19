d636871fcfdd1ba297f67127e4b993300b9c3b5d
= <shou.hao.fu@ibm.com>
2021-11-19 14:04:54 +0800

    feat(tutorial): complete step 2

--- src/views/RepoPage/RepoPage.vue
+++ src/views/RepoPage/RepoPage.vue
@@ -1,75 +1,75 @@
 <template>
-  <repo-table
-    :headers="headers"
-    :rows="rows"
-    title="Carbon Repositories"
-    helperText="A collection of public Carbon repositories."
-  />
-    <template slot="expandedContent">
-    {{ row.description }}
-  </template>
-  </cv-data-table>
+<div class="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+    <div class="bx--row repo-page__r1">
+      <repo-table
+  :headers="headers"
+  :rows="rows"
+  title="Carbon Repositories"
+  helperText="A collection of public Carbon repositories."
+/>
+    </div>
+  </div>
 </template>
 <script>
 import RepoTable from "./RepoTable";
 const headers = [
   {
-    key: "name",
-    header: "Name",
+    key: 'name',
+    header: 'Name',
   },
   {
-    key: "createdAt",
-    header: "Created",
+    key: 'createdAt',
+    header: 'Created',
   },
   {
-    key: "updatedAt",
-    header: "Updated",
+    key: 'updatedAt',
+    header: 'Updated',
   },
   {
-    key: "issueCount",
-    header: "Open Issues",
+    key: 'issueCount',
+    header: 'Open Issues',
   },
   {
-    key: "stars",
-    header: "Stars",
+    key: 'stars',
+    header: 'Stars',
   },
   {
-    key: "links",
-    header: "Links",
+    key: 'links',
+    header: 'Links',
   },
 ];
 
 const rows = [
   {
-    id: "1",
-    name: "Repo 1",
-    createdAt: "Date",
-    updatedAt: "Date",
-    issueCount: "123",
-    stars: "456",
-    links: "Links",
+    id: '1',
+    name: 'Repo 1',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links',
   },
   {
-    id: "2",
-    name: "Repo 2",
-    createdAt: "Date",
-    updatedAt: "Date",
-    issueCount: "123",
-    stars: "456",
-    links: "Links",
+    id: '2',
+    name: 'Repo 2',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links',
   },
   {
-    id: "3",
-    name: "Repo 3",
-    createdAt: "Date",
-    updatedAt: "Date",
-    issueCount: "123",
-    stars: "456",
-    links: "Links",
+    id: '3',
+    name: 'Repo 3',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links',
   },
 ];
 export default {
-  name: "RepoPage",
+  name: 'RepoPage',
   components: { RepoTable },
   data() {
     return {
@@ -77,11 +77,10 @@ export default {
       rows,
     };
   },
-  
 };
 </script>
 <style lang="scss">
-@import "../../styles/carbon-utils";
+@import '../../styles/carbon-utils';
 
 .repo-page .bx--row {
   padding-top: $spacing-05;
