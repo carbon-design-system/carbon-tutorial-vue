d264ce7f08a2b301c7b517489309acc18b68cb2f
= <shou.hao.fu@ibm.com>
2021-11-19 11:39:39 +0800

    feat(tutorial): complete step 2

--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,17 +1,18 @@
 <template>
   <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
-  <template slot="data">
-  <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`"> </cv-data-table-row>
-</template>
-<cv-data-table-cell
-  v-for="(cell, cellIndex) in row.data"
-  :key="`${cellIndex}`"
-  > <template slot="expandedContent">
-     {{ row.description }}
-   </template></cv-data-table-cell
->
+    <template slot="data">
+      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
+      </cv-data-table-row>
+    </template>
+    <cv-data-table-cell
+      v-for="(cell, cellIndex) in row.data"
+      :key="`${cellIndex}`"
+    >
+      <template slot="expandedContent">
+        {{ row.description }}
+      </template></cv-data-table-cell
+    >
   </cv-data-table>
-  
 </template>
 <script>
   export default {
