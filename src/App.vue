2ff6d01da451d1f1fa7bb3d9de30e4aac6f79828
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-11-25 15:59:17 -0300

    feat(tutorial): complete step 1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="app">
-    Hello Carbon! Well, not quite yet. This is the starting point for the Carbon
-    tutorial.
+    <TutorialHeader />
+    <CvContent id="#main-content">
+      <RouterView />
+    </CvContent>
   </div>
 </template>
 
-<style lang="scss"></style>
+<script>
+import TutorialHeader from './components/TutorialHeader';
+
+export default {
+  name: 'App',
+  components: {
+    TutorialHeader
+  }
+};
+</script>
+
+<style lang="scss">
+@import './styles/carbon';
+</style>
---
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <CvHeader aria-label="Carbon tutorial">
+    <CvSkipToContent href="#main-content">
+      Skip to content
+    </CvSkipToContent>
+    <CvHeaderName to="/" prefix="IBM">
+      Carbon tutorial
+    </CvHeaderName>
+    <CvHeaderNav>
+      <CvHeaderMenuItem to="/repos">
+        Repositories
+      </CvHeaderMenuItem>
+    </CvHeaderNav>
+    <template slot="header-global">
+      <CvHeaderGlobalAction aria-label="Notifications">
+        <Notification20 />
+      </CvHeaderGlobalAction>
+      <CvHeaderGlobalAction aria-label="User avatar">
+        <UserAvatar20 />
+      </CvHeaderGlobalAction>
+      <CvHeaderGlobalAction aria-label="App Switcher">
+        <AppSwitcher20 />
+      </CvHeaderGlobalAction>
+    </template>
+  </CvHeader>
+</template>
+
+<script>
+import { Notification20, UserAvatar20, AppSwitcher20 } from '@carbon/icons-vue';
+
+export default {
+  name: 'TutorialHeader',
+  components: {
+    Notification20,
+    UserAvatar20,
+    AppSwitcher20
+  }
+};
+</script>
