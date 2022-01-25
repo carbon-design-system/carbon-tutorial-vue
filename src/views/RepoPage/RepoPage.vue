7a86347adcb9e7dc1003783aed93c232e153ede5
Mihir Haresh <Mihir.Haresh-CIC@ibm.com>
2022-01-25 12:21:43 +0000

    feat(tutorial): complete step 2

--- src/views/RepoPage/RepoPage.vue
+++ src/views/RepoPage/RepoPage.vue
@@ -2,7 +2,7 @@
   <div class="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
     <div class="bx--row repo-page__r1">
       <div class="bx--col-lg-16">
-        <repo-table
+        <RepoTable
           :headers="headers"
           :rows="rows"
           title="Carbon Repositories"
@@ -84,3 +84,12 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+@import '../../styles/carbon-utils';
+
+.repo-page .bx--row {
+  padding-top: $spacing-05;
+  padding-bottom: $spacing-05;
+}
+</style>
--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,13 +1,13 @@
 <template>
   <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
-    <template v-slot:data>
+    <template slot="data">
       <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
         <cv-data-table-cell
           v-for="(cell, cellIndex) in row.data"
           :key="`${cellIndex}`"
           >{{ cell }}</cv-data-table-cell
         >
-        <template v-slot:expandedContent> {{ row.description }} xx </template>
+        <template slot="expandedContent">{{ row.description }}</template>
       </cv-data-table-row>
     </template>
   </cv-data-table>
@@ -43,11 +43,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-@import '../../styles/carbon-utils';
-
-.repo-page .bx--row {
-  padding-top: $spacing-05;
-  padding-bottom: $spacing-05;
-}
-</style>
+<style lang="scss"></style>
