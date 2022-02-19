f6632e1e2a577c3065cd29a9ef1805671208964e
Mateus Devino <mateus@mdevino.com>
2022-02-19 12:45:17 -0300

    Add Carbon Button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    Hello Carbon! Well, not quite yet. This is the starting point for the Carbon
-    tutorial.
+    <cv-button>Button</cv-button>
   </div>
 </template>
 
