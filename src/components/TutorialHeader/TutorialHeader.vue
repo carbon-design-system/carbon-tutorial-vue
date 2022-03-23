90b59fbaf6779daf09a1140b90463026f4f49aee
Henrique Fonseca <henriquefons@hotmail.com>
2022-03-23 14:46:31 -0300

    Feaxed: add tag 'cv-header-nav'

--- src/components/TutorialHeader/TutorialHeader.vue
+++ src/components/TutorialHeader/TutorialHeader.vue
@@ -5,7 +5,10 @@
     </cv-skip-to-content>
 
     <cv-header-name to="/" prefix="IBM">Carbon Tutorial</cv-header-name>
-    <cv-header-menu-item to="/repos">Repositories</cv-header-menu-item>
+
+    <cv-header-nav>
+      <cv-header-menu-item to="/repos">Repositories</cv-header-menu-item>
+    </cv-header-nav>
 
     <template slot="header-global">
       <cv-header-global-action aria-label="Notifications">
