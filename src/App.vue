1fd7eab47e2bbe6e797203a7382985038951d1a4
Johann Santos <Johann.Santos-CIC@ibm.com>
2022-03-24 11:26:20 +0000

    feat(tutorial): complete step 1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    Hello Carbon! Well, not quite yet. This is the starting point for the Carbon
-    tutorial.
+    <cv-content id="#main-content">
+      <router-view />
+    </cv-content>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import './styles/carbon';
+</style>
