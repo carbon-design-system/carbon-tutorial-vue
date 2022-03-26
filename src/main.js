257238ea6c3439c582ca549f8fc158405ac6da4b
Ilson Luiz de Oliveira Junior <ilsonlojr@ibm.com>
2022-03-26 00:33:56 -0300

    feat(tutorial): complete step 3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import { createProvider } from './vue-apollo';
 
 import CarbonComponentsVue from '@carbon/vue';
 Vue.use(CarbonComponentsVue);
@@ -9,5 +10,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app');
