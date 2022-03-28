0fdf95159008b06a3f2b47fe7f168a73620c87ca
Johann Santos <Johann.Santos-CIC@ibm.com>
2022-03-28 10:45:58 +0100

    feat(tutorial): complete step 3

--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,26 +1,47 @@
 <template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
-    <template v-slot:data>
-      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
+  <cv-data-table-skeleton
+    v-if="loading"
+    :columns="columns"
+    :title="title"
+    :helper-text="helperText"
+    :rows="10"
+  />
+  <cv-data-table
+    v-else
+    :columns="columns"
+    :title="title"
+    :helper-text="helperText"
+    :pagination="{ numberOfItems: this.totalRows }"
+    @pagination="$emit('pagination', $event)"
+  >
+    <template slot="data">
+      <cv-data-table-row v-for="row in data" :key="`${row.key}`">
         <cv-data-table-cell
           v-for="(cell, cellIndex) in row.data"
           :key="`${cellIndex}`"
-          >{{ cell }}</cv-data-table-cell
         >
-        <template v-slot:expandedContent> {{ row.description }} xx </template>
+          <template v-if="!cell.url">{{ cell }}</template>
+          <link-list v-else :url="cell.url" :homepage-url="cell.homepageUrl" />
+        </cv-data-table-cell>
+        <template slot="expandedContent">{{ row.description }}</template>
       </cv-data-table-row>
     </template>
   </cv-data-table>
 </template>
 
 <script>
+import LinkList from './LinkList';
+
 export default {
   name: 'RepoTable',
+  components: { LinkList },
   props: {
     headers: Array,
     rows: Array,
     title: String,
-    helperText: String
+    helperText: String,
+    loading: Boolean,
+    totalRows: Number
   },
   computed: {
     columns() {
@@ -36,18 +57,12 @@ export default {
           row.stars,
           row.links
         ],
-        description: 'Row description'
+        description: row.description,
+        key: row.key
       }));
     }
   }
 };
 </script>
 
-<style lang="scss">
-@import '../../styles/carbon-utils';
-
-.repo-page .bx--row {
-  padding-top: $spacing-05;
-  padding-bottom: $spacing-05;
-}
-</style>
+<style lang="scss"></style>
