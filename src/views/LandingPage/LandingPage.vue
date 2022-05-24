ccf53f440de7b29c396c7c2152e208139953bbc0
GUILHERME SILVA NUNES <=>
2022-05-24 12:05:13 -0300

    feat(tutorial): complete step 2

--- src/views/LandingPage/LandingPage.vue
+++ src/views/LandingPage/LandingPage.vue
@@ -1,3 +1,126 @@
 <template>
-  <div>LANDING PAGE</div>
+  <div class="cds--grid cds--grid--full-width landing-page">
+    <div class="cds--row landing-page__banner">
+  <div class="cds--col-lg-16">
+    <cv-breadcrumb noTrailingSlash aria-label="Page navigation">
+      <cv-breadcrumb-item>
+        <cv-link href="/">Getting started</cv-link>
+      </cv-breadcrumb-item>
+    </cv-breadcrumb>
+    <h1 class="landing-page__heading">Design &amp; build with Carbon</h1>
+  </div>
+</div>
+    <div class="cds--row landing-page__r2">
+  <div class="cds--col cds--no-gutter">
+    <cv-tabs selected="0" aria-label="Tab navigation">
+      <cv-tab label="About">
+  <div class="cds--grid cds--grid--no-gutter cds--grid--full-width">
+    <div class="cds--row landing-page__tab-content">
+      <div class="cds--col-md-4 cds--col-lg-7">
+        <h2 class="landing-page__subheading">What is Carbon?</h2>
+        <p class="landing-page__p">
+          Carbon is IBM’s open-source design system for digital
+          products and experiences. With the IBM Design Language as
+          its foundation, the system consists of working code, design
+          tools and resources, human interface guidelines, and a
+          vibrant community of contributors.
+        </p>
+        <cv-button>Learn more</cv-button>
+      </div>
+      <div class="cds--col-md-4 cds--offset-lg-1 cds--col-lg-8">
+        <img
+          class="landing-page__illo"
+          src="../../assets/tab-illo.png"
+          alt="Carbon illustration"
+        />
+      </div>
+    </div>
+  </div>
+</cv-tab>
+      <cv-tab label="Design">
+        <div class="cds--grid cds--grid--no-gutter cds--grid--full-width">
+          <div class="cds--row landing-page__tab-content">
+            <div class="cds--col-lg-16">
+              Rapidly build beautiful and accessible experiences. The Carbon kit
+              contains all resources you need to get started.
+            </div>
+          </div>
+        </div>
+      </cv-tab>
+      <cv-tab label="Develop">
+        <div class="cds--grid cds--grid--no-gutter cds--grid--full-width">
+          <div class="cds--row landing-page__tab-content">
+            <div class="cds--col-lg-16">
+              Carbon provides styles and components in Vanilla, React, Angular,
+              and Vue for anyone building on the web.
+            </div>
+          </div>
+        </div>
+      </cv-tab>
+    </cv-tabs>
+  </div>
+</div>
+    <div class="cds--row landing-page__r3">
+  <div class="cds--col-md-4 cds--col-lg-4">
+    <h3 class="landing-page__label">The Principles</h3>
+  </div>
+  <div class="cds--col-md-4 cds--col-lg-4">Carbon is Open</div>
+  <div class="cds--col-md-4 cds--col-lg-4">Carbon is Modular</div>
+  <div class="cds--col-md-4 cds--col-lg-4">Carbon is Consistent</div>
+</div>
+  </div>
 </template>
+
+<style lang="scss">
+@import "./carbon-overrides";
+@import '../../styles/carbon-utils';
+@import './mixins';
+
+.landing-page__illo {
+  max-width: 100%;
+}
+
+.landing-page__banner {
+  padding-top: $spacing-05;
+  padding-bottom: $spacing-07 * 4;
+  @include landing-page-background;
+}
+
+.landing-page__heading {
+  @include type-style('productive-heading-05');
+}
+
+.landing-page__r2 {
+  margin-top: rem(-40px);
+}
+
+.landing-page__tab-content {
+  padding-top: $layout-05;
+  padding-bottom: $layout-05;
+}
+
+.landing-page__subheading {
+  @include type-style('productive-heading-03');
+  //@include font-weight('semibold');
+}
+
+/*.landing-page__p {
+  @include type-style('productive-heading-03');
+  margin-top: $spacing-06;
+  margin-bottom: $spacing-08;
+
+  @include breakpoint-between((320px + 1), md) {
+    max-width: 75%;
+  }
+}*/
+
+.landing-page__r3 {
+  padding-top: $spacing-09;
+  padding-bottom: $spacing-09;
+  @include landing-page-background;
+}
+
+.landing-page__label {
+  @include type-style('heading-01');
+}
+</style>
