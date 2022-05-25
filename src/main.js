40b816968ecc3d7bbf12004ccdb9d76adf479440
Luis Paulo Rodrigues da Silva Lima <luis.lima@ibm.com>
2022-05-25 00:37:51 -0300

    feat(tutorial): complete step 3

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue';
+import CarbonComponentsVue from '@carbon/vue';
+import { createProvider } from './vue-apollo';
+
 import App from './App.vue';
 import router from './router';
 
-import CarbonComponentsVue from '@carbon/vue';
 Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app');
--- src/views/RepoPage/RepoTable.vue
+++ src/views/RepoPage/RepoTable.vue
@@ -1,26 +1,49 @@
 <template>
-  <cv-data-table :columns="columns" :title="title" :helper-text="helperText">
+  <cv-data-table-skeleton
+    v-if="loading"
+    :columns="columns"
+    :title="title"
+    :helper-text="helperText"
+    :rows="10"
+  />
+  <cv-data-table
+    v-else
+    :columns="columns"
+    :title="title"
+    :helper-text="helperText"
+    :pagination="{ numberOfItems: this.totalRows }"
+    @pagination="$emit('pagination', $event)"
+  >
     <template v-slot:data>
-      <cv-data-table-row v-for="(row, rowIndex) in data" :key="`${rowIndex}`">
+      <cv-data-table-row v-for="row in data" :key="row.id">
         <cv-data-table-cell
           v-for="(cell, cellIndex) in row.data"
           :key="`${cellIndex}`"
-          >{{ cell }}</cv-data-table-cell
         >
-        <template v-slot:expandedContent> {{ row.description }} xx </template>
+          <template v-if="!cell.url">
+            {{ cell }}
+          </template>
+          <link-list v-else :url="cell.url" :homepage-url="cell.homepageUrl" />
+        </cv-data-table-cell>
+        <template v-slot:expandedContent> {{ row.description }} </template>
       </cv-data-table-row>
     </template>
   </cv-data-table>
 </template>
 
 <script>
+import LinkList from './LinkList';
+
 export default {
   name: 'RepoTable',
+  components: { LinkList },
   props: {
     headers: Array,
     rows: Array,
     title: String,
-    helperText: String
+    helperText: String,
+    loading: Boolean,
+    totalRows: Number
   },
   computed: {
     columns() {
@@ -36,7 +59,8 @@ export default {
           row.stars,
           row.links
         ],
-        description: 'Row description'
+        id: row.id,
+        description: row.description
       }));
     }
   }
