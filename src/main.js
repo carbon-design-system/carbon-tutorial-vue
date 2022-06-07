122b9329ab1882e3e3ed315316662567f5a56564
8bkgr <8sbkgr@gmail.com>
2022-06-07 15:29:34 +1200

    feat(tutorial): complete step 1

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import CarbonComponentsVue from "@carbon/vue";
-Vue.use(CarbonComponentsVue);
 
+Vue.use(CarbonComponentsVue);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app');
