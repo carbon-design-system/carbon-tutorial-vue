83ee8789ed8af6eaa9042ab47f47da866a0a2296
Shi Bo Yan <Shibo.Yan@ibm.com>
2022-06-14 19:22:08 +0800

    feat(tutorial): complete step 3

--- src/views/RepoPage/RepoPage.vue
+++ src/views/RepoPage/RepoPage.vue
@@ -2,7 +2,7 @@
   <div class="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
     <div class="bx--row repo-page__r1">
       <div class="bx--col-lg-16">
-        <!-- {{ this.organization }} -->
+        {{ this.organization }}
         <repo-table :headers="headers" title="Carbon Repositories"
           helperText="A collection of public Carbon repositories." :loading="$apollo.loading" :rows="pagedRows"
           :totalRows="rows.length" @pagination="onPagination" />
