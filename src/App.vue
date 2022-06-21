ff14c58808119073ff5ea9483a575101fb07b503
jerzonhr <106408622+jerzonhr@users.noreply.github.com>
2022-06-21 12:27:53 -0600

    feat(tutorial): complete step 1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
   <div id="app">
-    Hello Carbon! Well, not quite yet. This is the starting point for the Carbon
-    tutorial.
+    <tutorial-header />
+    <cv-content id="#main-content">
+      <router-view />
+      <CvButton>Button</CvButton>
+    </cv-content>
   </div>
 </template>
 
-<style lang="scss"></style>
+<script>
+import TutorialHeader from './components/TutorialHeader';
+
+export default {
+  name: 'App',
+  components: {
+    TutorialHeader
+  }
+};
+</script>
+<style>
+/* Testing to overwrite css */
+/* .bx--content button {
+  background-color: brown !important;
+} */
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import CarbonComponentsVue from '@carbon/vue';
+import 'carbon-components/css/carbon-components.min.css';
+Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false;
 
