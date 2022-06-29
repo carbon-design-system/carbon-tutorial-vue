0a8a8792690a855695864cf114fc7ad43fcdaff7
Larissa Kelly <Larissa.Silva2@ibm.com>
2022-06-29 14:56:24 -0300

    Revert "feat(tutorial): complete step 1"
    This reverts commit 0a4ecd5ceb23f3164b15a0f22643cd1e3e44a735.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import CarbonComponentsVue from '@carbon/vue';
-Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false;
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import LandingPage from './views/LandingPage';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -8,17 +8,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: LandingPage
+      name: 'home',
+      component: Home
     },
     {
-      path: '/repos',
-      name: 'repo-page',
+      path: '/about',
+      name: 'about',
       // route level code-splitting
-      // this generates a separate chunk (repo-page.[hash].js) for this route
+      // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "repo-page" */ './views/RepoPage')
+        import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 });
