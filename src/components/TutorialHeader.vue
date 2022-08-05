791c09c01da3b5bdcef483f72bd7e296c4798df1
Andy-Mair <andycmair98@gmail.com>
2022-08-05 15:42:21 +0100

    tutorial step 1

---
+++ src/components/TutorialHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <CvHeader aria-label="Carbon tutorial">
+    <CvSkipToContent href="#main-content">Skip to content!</CvSkipToContent>
+
+    <CvHeaderName to="/" prefix="IBM">Carbon Tutorial</CvHeaderName>
+
+    <CvHeaderNav>
+      <CvHeaderMenuItem to="/repos">Repositories</CvHeaderMenuItem>
+    </CvHeaderNav>
+
+    <template slot="header-global">
+      <CvHeaderGlobalAction aria-label="Notifications">
+        <notification-20 />
+      </CvHeaderGlobalAction>
+      <CvHeaderGlobalAction aria-label="User Avatar">
+        <user-avatar-20 />
+      </CvHeaderGlobalAction>
+      <CvHeaderGlobalAction aria-label="App switcher">
+        <app-switcher-20 />
+      </CvHeaderGlobalAction>
+    </template>
+  </CvHeader>
+</template>
+
+<script>
+import { Notification20, UserAvatar20, AppSwitcher20 } from '@carbon/icons-vue';
+
+export default {
+  name: 'TutorialHeader',
+  components: { Notification20, UserAvatar20, AppSwitcher20 }
+};
+</script>
