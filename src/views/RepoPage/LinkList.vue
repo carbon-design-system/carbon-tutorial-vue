47fef28f82088bb43c03f913c4cf4beeb50a65d2
Davide Barletta <Davide.Barletta@ibm.com>
2022-09-21 12:55:32 +0200

    feat(tutorial): complete step 3

---
+++ src/views/RepoPage/LinkList.vue
@@ -0,0 +1,18 @@
+<ul class="link-list">
+  <li>
+    <cv-link :href="url">GitHub</cv-link>
+  </li>
+
+  <li v-if="homepageUrl">
+    <span>&nbsp;|&nbsp;</span>
+    <cv-link :href="homepageUrl">Homepage</cv-link>
+  </li>
+</ul>
+
+<script></script>
+
+<style lang="scss">
+.link-list {
+  display: flex;
+}
+</style>
