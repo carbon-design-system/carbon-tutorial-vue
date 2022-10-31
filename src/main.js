58158c93f79eb6b7ffdca77817e4a3d00279bc56
Sabrina Izabel dos Santos Silva <Sabrina.Silva@ibm.com>
2022-10-31 19:50:52 -0300

    feat(tutorial): complete step 1

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import CarbonComponentsVue from '@carbon/vue';
+Vue.use(CarbonComponentsVue);
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app');
