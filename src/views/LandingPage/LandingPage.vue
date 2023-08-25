4b4dc96d52fe49cc7f2ef9d9d64ef10a1b556176
Alvaro Prataviera Filho <alvaro@li-72736bcc-2847-11b2-a85c-f75f51631753.ibm.com>
2023-08-25 15:45:56 -0300

    feat(tutorial): complete step 2

--- src/views/LandingPage/LandingPage.vue
+++ src/views/LandingPage/LandingPage.vue
@@ -61,18 +61,66 @@
       </div>
     </div>
     <div class="cds--row landing-page__r3">
-      <div class="cds--col-md-4 cds--col-lg-4">1/4</div>
-      <div class="cds--col-md-4 cds--col-lg-4">1/4</div>
-      <div class="cds--col-md-4 cds--col-lg-4">1/4</div>
-      <div class="cds--col-md-4 cds--col-lg-4">1/4</div>
+      <div class="cds--col-md-4 cds--col-lg-4">
+        <h3 class="landing-page__label">The Principles</h3>
+      </div>
+      <div class="cds--col-md-4 cds--col-lg-4">Carbon is Open</div>
+      <div class="cds--col-md-4 cds--col-lg-4">Carbon is Modular</div>
+      <div class="cds--col-md-4 cds--col-lg-4">Carbon is Consistent</div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+@import '../../styles/carbon-utils';
 @import './carbon-overrides';
+@import './mixins';
 
 .landing-page__illo {
   max-width: 100%;
+}
+
+.landing-page__banner {
+  padding-top: $spacing-05;
+  padding-bottom: $spacing-07 * 4;
+  @include landing-page-background;
+}
+
+.landing-page__heading {
+  @include type-style('productive-heading-05');
+}
+
+.landing-page__r2 {
+  margin-top: rem(-40px);
+}
+
+.landing-page__tab-content {
+  padding-top: $layout-05;
+  padding-bottom: $layout-05;
+}
+
+.landing-page__subheading {
+  @include type-style('productive-heading-03');
+  @include font-weight('semibold');
+}
+
+.landing-page__p {
+  @include type-style('productive-heading-03');
+  margin-top: $spacing-06;
+  margin-bottom: $spacing-08;
+
+  @include breakpoint-between((320px + 1), md) {
+    max-width: 75%;
+  }
+}
+
+.landing-page__r3 {
+  padding-top: $spacing-09;
+  padding-bottom: $spacing-09;
+  @include landing-page-background;
+}
+
+.landing-page__label {
+  @include type-style('heading-01');
 }
 </style>
